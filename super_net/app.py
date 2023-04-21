c3456aa1888c3329118a5e6b5ea2fa904ab77878
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-04-21 09:14:45 +0100

    inherit app and config from validphys

--- super_net/app.py
+++ super_net/app.py
@@ -1,15 +1,17 @@
-from reportengine.app import App
-from reportengine.report import Config
+from validphys.app import App
+from super_net.config import SuperNetConfig
 
-class SuperNetConfig(Config):
-    def produce_example(self):
-        return "example"
+
+providers = [
+    "reportengine.report",
+    "super_net.tmp"
+]
 
 class SuperNetApp(App):
     config_class = SuperNetConfig
 
 def main():
-    a = SuperNetApp(name='super_net', default_providers=['reportengine.report'])
+    a = SuperNetApp(name='super_net', providers=providers)
     a.main()
 
 if __name__ == '__main__':
---
+++ super_net/config.py
@@ -0,0 +1,12 @@
+from validphys.config import Config
+
+
+class SuperNetConfig(Config):
+    """
+    TODO
+    """
+    def produce_example(self):
+        return "example"
+    
+    # def produce_data(self):
+    #     print("ciao")
