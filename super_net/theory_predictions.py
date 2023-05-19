ed68a98a8413c2c00559b9e6a227b40a30c7a2b3
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-05-19 18:28:13 +0100

    make prediction for single dataset

--- super_net/theory_predictions.py
+++ super_net/theory_predictions.py
@@ -1,6 +1,12 @@
 import jax
 import jax.numpy as jnp
 
+from validphys import convolution
+from validphys.fkparser import load_fktable
+
+
+# Is this needed? -> probably no need to jit compile
+OP = {key: jax.jit(val) for key,val in convolution.OP.items()}
 
 def make_dis_prediction(fk):
     """
@@ -51,3 +57,37 @@ def make_had_prediction(fk):
     def had_prediction(pdf):
         return jnp.einsum("ijkl,jk,jl->i", fk_arr, pdf[first_indices,:], pdf[second_indices,:])
     return had_prediction
+
+
+def make_pred_dataset(dataset):
+        """
+        Compute theory prediction for a DataSetSpec
+
+        Parameters
+        ----------
+        dataset : validphys.core.DataSetSpec
+
+        Returns
+        -------
+        @jax.jit CompiledFunction
+            Compiled function taking pdf grid in input
+            and returning theory prediction for one
+            dataset
+        """
+        
+        pred_funcs = []
+        
+        for fkspec in dataset.fkspecs:
+            
+            fk = load_fktable(fkspec).with_cuts(dataset.cuts)
+            if fk.hadronic:
+                pred = make_had_prediction(fk)
+            else:
+                pred = make_dis_prediction(fk)
+            pred_funcs.append(pred)
+            
+        @jax.jit
+        def prediction(pdf):
+            return OP[dataset.op](*[f(pdf) for f in pred_funcs])
+        
+        return prediction
