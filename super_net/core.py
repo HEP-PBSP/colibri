a5ed582597715b2a41da70fd30797972ec665197
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-06-15 09:34:23 +0100

    added logic for MC fit

--- super_net/core.py
+++ super_net/core.py
@@ -10,6 +10,7 @@ from validphys import convolution
 from validphys.fkparser import load_fktable
 from validphys.core import DataSetSpec, DataGroupSpec
 from validphys import covmats
+from validphys.pseudodata import make_level1_data
 
 from super_net.theory_predictions import (
     make_dis_prediction,
@@ -62,6 +63,52 @@ class NewDataGroupSpec(DataGroupSpec):
     Class inheriting from `validphys.core.DataGroupSpec`
     """
 
+    def data_index(self):
+        """
+        index of data, needed by `make_replica`
+        """
+        tuples = []
+        for ds in self.datasets:
+            for i in ds.cuts.load():
+                tp = ("test", ds.name, i)
+                tuples.append(tp)
+        return pd.MultiIndex.from_tuples(tuples, names=("group", "dataset", "id"))
+
+    def load_pseudo_commondata(self, replicate=False, filterseed=1):
+        """
+        Like `load_commondata_instance` but allowing for the generation
+        of pseudodata trough make_level1_data function
+
+        Parameters
+        ----------
+        replicate : bool, default is False
+                    when False, load_commondata_instance, i.e. the experimental
+                    commondata is returned. Otherwise noise is added to the
+                    central values of the commondata
+        filterseed : int, default is 1
+                    seed used for the sampling of random noise
+
+        Returns
+        -------
+        list
+            list of commondata
+        """
+
+        if not replicate:
+            return self.load_commondata_instance()
+
+        else:
+            index = self.data_index()
+            experimental_data = self.load_commondata_instance()
+            dataset_order = [cd.setname for cd in experimental_data]
+            pseudodata = make_level1_data(
+                self, experimental_data, filterseed, index, sep_mult=True
+            )
+            pseudodata = sorted(
+                pseudodata, key=lambda obj: dataset_order.index(obj.setname)
+            )
+            return pseudodata
+
     @property
     def make_pred_data(self):
         """
@@ -85,12 +132,14 @@ class NewDataGroupSpec(DataGroupSpec):
 
         return eval_preds
 
-    @property
-    def data_values(self):
+    def pseudo_data_values(self, replicate=False, filterseed=1):
         """
         Method of NewDataGroupSpec.
         Used to get data values, covariance matrix,
         and indices of data values.
+
+        If replicate is False, then the experimental data values are
+        returned.
 
         Returns
         -------
@@ -101,7 +150,9 @@ class NewDataGroupSpec(DataGroupSpec):
             - jnp.ndarray of indices of data values
         """
 
-        cd_list = self.load_commondata_instance()
+        cd_list = self.load_pseudo_commondata(
+            replicate=replicate, filterseed=filterseed
+        )
 
         central_values = [cd.central_values for cd in cd_list]
 
@@ -117,7 +168,9 @@ class NewDataGroupSpec(DataGroupSpec):
 
         return central_values, covmat, indices
 
-    def train_validation_split(self, test_size=0.2, random_state=42):
+    def train_validation_split(
+        self, test_size=0.2, random_state=42, replicate=False, filterseed=1
+    ):
         """
         Get training validation split for the data values.
 
@@ -142,7 +195,9 @@ class NewDataGroupSpec(DataGroupSpec):
             - jnp.ndarray of validation indices
         """
 
-        central_values, covmat, indices = self.data_values
+        central_values, covmat, indices = self.pseudo_data_values(
+            replicate=replicate, filterseed=filterseed
+        )
 
         # Perform train-test split on indices
         indices_train, indices_val = train_test_split(
@@ -170,7 +225,12 @@ class NewDataGroupSpec(DataGroupSpec):
 
     # @property
     def make_chi2_data_mini_batch(
-        self, train_val_split=False, test_size=0.2, random_state=42
+        self,
+        train_val_split=False,
+        test_size=0.2,
+        random_state=42,
+        replicate=False,
+        filterseed=1,
     ):
         """
         Compute the chi2 between experimental central values
@@ -196,10 +256,15 @@ class NewDataGroupSpec(DataGroupSpec):
                 _,
                 _,
             ) = self.train_validation_split(
-                test_size=test_size, random_state=random_state
+                test_size=test_size,
+                random_state=random_state,
+                replicate=replicate,
+                filterseed=filterseed,
             )
         else:
-            central_values_train, covmat_train, indices_train = self.data_values
+            central_values_train, covmat_train, indices_train = self.pseudo_data_values(
+                replicate=replicate, filterseed=filterseed
+            )
 
         pred = self.make_pred_data
 
@@ -228,7 +293,9 @@ class NewDataGroupSpec(DataGroupSpec):
 
         return chi2
 
-    def make_chi2_validation(self, test_size=0.2, random_state=42):
+    def make_chi2_validation(
+        self, test_size=0.2, random_state=42, replicate=False, filterseed=1
+    ):
         """
         Compute the chi2 on the validation set. The chi2 is
         computed between experimental central values
@@ -250,14 +317,18 @@ class NewDataGroupSpec(DataGroupSpec):
             central_values_val,
             covmat_val,
             indices_val,
-        ) = self.train_validation_split(test_size=test_size, random_state=random_state)
+        ) = self.train_validation_split(
+            test_size=test_size,
+            random_state=random_state,
+            replicate=replicate,
+            filterseed=filterseed,
+        )
 
         pred = self.make_pred_data
 
         @jax.jit
         def chi2(pdf):
             """
-            Compute batched chi2
 
             Parameters
             ----------
