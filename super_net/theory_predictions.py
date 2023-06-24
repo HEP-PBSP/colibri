89945ddb55bc8a35970f0af79187d3d740c371b3
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-06-24 22:15:41 +0100

    theory pred providers

--- super_net/theory_predictions.py
+++ super_net/theory_predictions.py
@@ -1,3 +1,5 @@
+import itertools
+
 import jax
 import jax.numpy as jnp
 
@@ -6,26 +8,27 @@ from validphys.fkparser import load_fktable
 
 
 # Is this needed? -> probably no need to jit compile
-OP = {key: jax.jit(val) for key,val in convolution.OP.items()}
+OP = {key: jax.jit(val) for key, val in convolution.OP.items()}
 
-def make_dis_prediction(fk):
+
+def make_dis_prediction(fktable):
     """
     given an FKTableData instance returns a jax.jit
     compiled function taking a pdf grid as input
     and returning a theory prediction for a DIS
     observable.
-    
+
     Parameters
     ----------
-    fk : validphys.coredata.FKTableData
-    
+    fktable : validphys.coredata.FKTableData
+
     Returns
     -------
-    @jax.jit CompiledFunction 
+    @jax.jit CompiledFunction
     """
-    indices = fk.luminosity_mapping
-    fk_arr = jnp.array(fk.get_np_fktable())
-    
+    indices = fktable.luminosity_mapping
+    fk_arr = jnp.array(fktable.get_np_fktable())
+
     @jax.jit
     def dis_prediction(pdf):
         return jnp.einsum("ijk, jk ->i", fk_arr, pdf[indices, :])
@@ -33,61 +36,92 @@ def make_dis_prediction(fk):
     return dis_prediction
 
 
-def make_had_prediction(fk):
+def make_had_prediction(fktable):
     """
     given an FKTableData instance returns a jax.jit
     compiled function taking a pdf grid as input
     and returning a theory prediction for a hadronic
     observable.
-    
+
     Parameters
     ----------
-    fk : validphys.coredata.FKTableData
-    
+    fktable : validphys.coredata.FKTableData
+
     Returns
     -------
-    @jax.jit CompiledFunction 
+    @jax.jit CompiledFunction
     """
 
-    indices = fk.luminosity_mapping
+    indices = fktable.luminosity_mapping
     first_indices = indices[0::2]
     second_indices = indices[1::2]
-    fk_arr = jnp.array(fk.get_np_fktable())
+    fk_arr = jnp.array(fktable.get_np_fktable())
+
     @jax.jit
     def had_prediction(pdf):
-        return jnp.einsum("ijkl,jk,jl->i", fk_arr, pdf[first_indices,:], pdf[second_indices,:])
+        return jnp.einsum(
+            "ijkl,jk,jl->i", fk_arr, pdf[first_indices, :], pdf[second_indices, :]
+        )
+
     return had_prediction
 
 
 def make_pred_dataset(dataset):
-        """
-        Compute theory prediction for a DataSetSpec
+    """
+    Compute theory prediction for a DataSetSpec
 
-        Parameters
-        ----------
-        dataset : validphys.core.DataSetSpec
+    Parameters
+    ----------
+    dataset : validphys.core.DataSetSpec
 
-        Returns
-        -------
-        @jax.jit CompiledFunction
-            Compiled function taking pdf grid in input
-            and returning theory prediction for one
-            dataset
-        """
-        
-        pred_funcs = []
-        
-        for fkspec in dataset.fkspecs:
-            
-            fk = load_fktable(fkspec).with_cuts(dataset.cuts)
-            if fk.hadronic:
-                pred = make_had_prediction(fk)
-            else:
-                pred = make_dis_prediction(fk)
-            pred_funcs.append(pred)
-            
-        @jax.jit
-        def prediction(pdf):
-            return OP[dataset.op](*[f(pdf) for f in pred_funcs])
-        
-        return prediction
+    Returns
+    -------
+    @jax.jit CompiledFunction
+        Compiled function taking pdf grid in input
+        and returning theory prediction for one
+        dataset
+    """
+
+    pred_funcs = []
+
+    for fkspec in dataset.fkspecs:
+        fk = load_fktable(fkspec).with_cuts(dataset.cuts)
+        if fk.hadronic:
+            pred = make_had_prediction(fk)
+        else:
+            pred = make_dis_prediction(fk)
+        pred_funcs.append(pred)
+
+    @jax.jit
+    def prediction(pdf):
+        return OP[dataset.op](*[f(pdf) for f in pred_funcs])
+
+    return prediction
+
+
+def make_pred_data(data):
+    """
+    Compute theory prediction for entire DataGroupSpec
+
+    Parameters
+    ----------
+    data: DataGroupSpec instance
+
+    Returns
+    -------
+    @jax.jit CompiledFunction
+        Compiled function taking pdf grid in input
+        and returning theory prediction for one
+        data group
+    """
+
+    predictions = []
+
+    for ds in data.datasets:
+        predictions.append(make_pred_dataset(ds))
+
+    @jax.jit
+    def eval_preds(pdf):
+        return jnp.array(list(itertools.chain(*[f(pdf) for f in predictions])))
+
+    return eval_preds
