39918702139472aaa42aa1982dd1b09fdca60d90
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-06-25 11:12:17 +0100

    checks module

---
+++ super_net/checks.py
@@ -0,0 +1,16 @@
+"""
+Module for super_net checks
+"""
+from reportengine.checks import make_check, CheckError
+
+@make_check
+def check_wminpdfset_is_montecarlo(ns, **kwargs):
+    """
+    same as `validphys.checks.check_pdf_is_montecarlo` but for
+    weight minimization set
+    """
+    
+    pdf = ns['wminpdfset']
+    etype = pdf.error_type
+    if etype != 'replicas':
+        raise CheckError(f"Error type of PDF {pdf} must be 'replicas' and not {etype}")
