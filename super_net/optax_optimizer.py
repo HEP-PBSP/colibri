dda589d9a2dd7209608c0c5751bd83ff87d6f6cb
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-07-18 20:18:39 +0100

    added weight_decay for adamw

--- super_net/optax_optimizer.py
+++ super_net/optax_optimizer.py
@@ -3,11 +3,18 @@ from flax.training.early_stopping import EarlyStopping
 
 
 def optimizer_provider(
-    optimizer: str = "adam", learning_rate: float = 5e-4
+    optimizer: str = "adam", learning_rate: float = 5e-4, weight_decay=2
 ) -> optax._src.base.GradientTransformationExtraArgs:
     """ """
     opt = getattr(optax, optimizer)
-    return opt(learning_rate=learning_rate)
+    kwargs = {"learning_rate": learning_rate}
+
+    # Check if the optimizer has the weight_decay argument
+    if "weight_decay" in opt.__code__.co_varnames:
+        kwargs["weight_decay"] = weight_decay
+
+    return opt(**kwargs)
+    
 
 
 def early_stopper(min_delta=1e-5, patience=20):
