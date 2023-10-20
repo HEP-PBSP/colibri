9320c0e548269304939e671f13a849755a53ecf4
J-M-Moore <james.m.moore@aol.co.uk>
2023-10-20 09:49:51 +0100

    Output folder now has same name as runcard, for ease of readability

--- super_net/wmin/wmin/app.py
+++ super_net/wmin/wmin/app.py
@@ -1,8 +1,10 @@
 """
 TODO
 """
-from super_net.app import SuperNetApp, providers
+from super_net.app import providers, SuperNetApp
 from wmin.config import WminConfig
+
+import pathlib
 
 wmin_providers = [
     *providers,
@@ -17,9 +19,28 @@ wmin_providers = [
 class WminApp(SuperNetApp):
     config_class = WminConfig
 
+    @property
+    def argparser(self):
+        parser = super().argparser
+
+        parser.add_argument(
+            '-o',
+            '--output',
+            nargs='?',
+            default=None,
+            help='Name of the output directory.',
+        )
+
+        return parser
+
+    def get_commandline_arguments(self, cmdline=None):
+        args = super().get_commandline_arguments(cmdline)
+        if args['output'] is None:
+            args['output'] = pathlib.Path(args['config_yml']).stem
+        return args
 
 def main():
-    a = WminApp(name="wmin", providers=wmin_providers)
+    a = WminApp(name='wmin', providers=wmin_providers)
     a.main()
 
 
