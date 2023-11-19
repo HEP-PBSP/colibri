bf24ebbacb4ad8c6c9d5a42bf538d88ddeeee771
Mark Nestor Costantini <mnc33@cam.ac.uk>
2023-11-19 12:22:07 +0000

    uniform and gaussian 1sigma band prior

--- super_net/ppdf/ppdf/ppdf_model.py
+++ super_net/ppdf/ppdf/ppdf_model.py
@@ -43,40 +43,11 @@ FLAVOURS_ID_MAPPINGS = {
 Dict, key = flavour id (see FLAVOURS_ID_MAPPINGS), val = reduced x grid.
 """
 REDUCED_XGRIDS = {
+    "length": 50,
     0: [],
-    1: [
-        2.00000000e-07,
-        2.43894329e-06,
-        1.96025050e-05,
-        2.38787829e-04,
-        2.87386758e-03,
-        6.04800288e-02,
-        2.65113704e-01,
-        7.29586844e-01,
-        9.29586844e-01,
-    ],
-    2: [
-        2.00000000e-07,
-        2.43894329e-06,
-        1.96025050e-05,
-        2.38787829e-04,
-        2.87386758e-03,
-        6.04800288e-02,
-        2.65113704e-01,
-        7.29586844e-01,
-        9.29586844e-01,
-    ],
-    3: [
-        2.00000000e-07,
-        2.43894329e-06,
-        1.96025050e-05,
-        2.38787829e-04,
-        2.87386758e-03,
-        6.04800288e-02,
-        2.65113704e-01,
-        7.29586844e-01,
-        9.29586844e-01,
-    ],
+    1: XGRID,
+    2: XGRID,
+    3: XGRID,
     4: [],
     5: [],
     6: [],
@@ -108,13 +79,12 @@ def interpolate_grid(stacked_pdf_grid, flavour_mapping=FLAVOUR_MAPPING):
 
     """
 
-    lenght_grid = jnp.sum(
-        jnp.array([len(REDUCED_XGRIDS[fl]) for fl in flavour_mapping])
-    )
+    # all reduced x grids should have the same length
+    length_grid = REDUCED_XGRIDS["length"]
 
     # reshape stacked_pdf_grid to (len(REDUCED_XGRID), len(flavour_mapping))
     reshaped_stacked_pdf_grid = stacked_pdf_grid.reshape(
-        (lenght_grid, len(flavour_mapping)), order="F"
+        (length_grid, len(flavour_mapping)), order="F"
     )
 
     # generate an empty matrix of shape (len(super_net.constants.XGRID),valipdhys.convolution.NFK)
@@ -135,13 +105,18 @@ def interpolate_grid(stacked_pdf_grid, flavour_mapping=FLAVOUR_MAPPING):
 
 @dataclass(frozen=True)
 class PdfPriorGrid:
+    """
+    TODO
+    """
+
     stacked_pdf_grid_prior: jnp.array
     pdf_covmat_prior: jnp.array
+    error68_up: jnp.array
+    error68_down: jnp.array
 
 
 def pdf_prior_grid(pdf_prior, flavour_mapping=FLAVOUR_MAPPING, Q0=1.65):
     """
-    TODO 
     Get PDF grid prior values (x*f(x)) from a PDF set.
 
     Parameters
@@ -157,18 +132,18 @@ def pdf_prior_grid(pdf_prior, flavour_mapping=FLAVOUR_MAPPING, Q0=1.65):
 
     Returns
     -------
-    jnp.array
-        1-D array of central values of prior PDF for the reduced
-        grid used in the fit. Flavours included in the fit are stacked.
-    TODO
+    PdfPriorGrid dataclass with the following attributes:
+        stacked_pdf_grid_prior: jnp.array
+        pdf_covmat_prior: jnp.array
+        error68_up: jnp.array
+        error68_down: jnp.array
     """
 
     stacked_pdf_grid_prior = jnp.array([])
 
-    # note: different flavours might have different len REDUCED_XGRID
+    # note: different flavours might have different REDUCED_XGRID
     for fl in flavour_mapping:
-        # save central value of pdf_prior
-
+        # Save central value of pdf_prior at Q0
         stacked_pdf_grid_prior = jnp.append(
             stacked_pdf_grid_prior,
             jnp.array(
@@ -186,46 +161,85 @@ def pdf_prior_grid(pdf_prior, flavour_mapping=FLAVOUR_MAPPING, Q0=1.65):
         [Q0],
     )
 
+    error68_up = jnp.nanpercentile(replicas_grid, 84.13, axis=0).reshape(-1)
+    error68_down = jnp.nanpercentile(replicas_grid, 15.87, axis=0).reshape(-1)
+
     pdf_covmat_prior = jnp.cov(replicas_grid.reshape((replicas_grid.shape[0], -1)).T)
-    
+
     return PdfPriorGrid(
-        stacked_pdf_grid_prior=stacked_pdf_grid_prior, pdf_covmat_prior=pdf_covmat_prior
+        stacked_pdf_grid_prior=stacked_pdf_grid_prior,
+        pdf_covmat_prior=pdf_covmat_prior,
+        error68_up=error68_up,
+        error68_down=error68_down,
     )
 
 
-def ppdf_model_prior(pdf_prior_grid, sigma):
+def ppdf_model_prior(
+    pdf_prior_grid, uniform_pdf_prior=True, gaussian_pdf_prior=False, sigma_pdf_prior=1
+):
     """
-    General Idea: given pdf_prior_grid return a grid
-    whose values are random gaussians centred at the prior
-    grid and with certain sigma.
+    This function returns a prior transform for the ultranest sampler.
 
-    Use Cholesky decomposition to generate a multivariate Gaussian
+    1. if uniform_pdf_prior is True, return a grid whose values are uniformly distribued 
+       in the 68% confidence interval of the central value of pdf_prior.
+    
+    2. if gaussian_pdf_prior is True, return a grid centered at the central value of the 
+       pdf_prior and with a covariance matrix given by the diagonal entries of covariance matrix 
+       of the pdf_prior
+
+
+    Note:
+        - The 68% confidence interval is computed from the replicas of the pdf_prior
+        - TODO: the 68% confidence interval should be modifiedable by the user
+
 
     Parameters
     ----------
-    pdf_prior:
+    pdf_prior_grid: PdfPriorGrid dataclass
+        contains the prior pdf grid values and the upper and lower 68% confidence intervals
 
     """
+    # This is needed to avoid ultranest crashing with
+    # ValueError: Buffer dtype mismatch, expected 'float_t' but got 'float'
+    jax.config.update("jax_enable_x64", True)
+
     stacked_pdf_grid_prior = pdf_prior_grid.stacked_pdf_grid_prior
-    pdf_covmat_prior = sigma * pdf_prior_grid.pdf_covmat_prior
 
-    cholesky_pdf_covmat = jnp.linalg.cholesky(pdf_covmat_prior)
+    if gaussian_pdf_prior:
+        pdf_covmat_prior = pdf_prior_grid.pdf_covmat_prior
 
-    # a = jnp.linalg.inv(pdf_covmat_prior)
-    # l, v = jnp.linalg.eigh(a)
-    # rotation_matrix = jnp.dot(v, jnp.diag(1. / jnp.sqrt(l)) )
+        # note: this matrix could be ill-conditioned when too many xgrids are used
+        # this is because xgrid values are not independent
+        # in order to avoid this we  only keep the diagonal of the covariance matrix
+        # and regularise it by cutting off small values
+        pdf_diag_covmat_prior = jnp.where(
+            jnp.diag(pdf_covmat_prior) < 0, 0, jnp.diag(pdf_covmat_prior)
+        )
+        cholesky_pdf_covmat = jnp.diag(jnp.sqrt(pdf_diag_covmat_prior))
 
-    def prior_transform(cube):
-        """
-        TODO
-        """
-        # sample an independent multivariate gaussian
-        independent_gaussian = jax.scipy.stats.norm.ppf(cube)
-        
-        # rotate and shift
-        return stacked_pdf_grid_prior + jnp.einsum("ij,kj->ki", independent_gaussian.T, cholesky_pdf_covmat).squeeze(-1)
-        
-        # return stacked_pdf_grid_prior + jnp.einsum("ij,kj->ki", independent_gaussian.T, rotation_matrix).squeeze(-1)
-        
+        def prior_transform(cube):
+            """
+            TODO
+            """
+            # generate independent gaussian with mean 0 and std 1
+            independent_gaussian = jax.scipy.stats.norm.ppf(cube)[:, jnp.newaxis]
+
+            # generate random samples from a multivariate normal distribution
+            prior = stacked_pdf_grid_prior + jnp.einsum(
+                "ij,kj->ki", independent_gaussian.T, cholesky_pdf_covmat
+            ).squeeze(-1)
+            return prior
+
+    elif uniform_pdf_prior:
+        error68_up = pdf_prior_grid.error68_up
+        error68_down = pdf_prior_grid.error68_down
+
+        def prior_transform(cube):
+            """
+            TODO
+            """
+            params = cube.copy()
+            params = error68_down + (error68_up - error68_down) * cube
+            return params
 
     return prior_transform
