807feb9da092455ca6257c80acf42378acea0194
Luca Mantani <luca.mantani@gmail.com>
2023-11-21 10:48:51 +0000

    Optimised generic chi2 calculation in loss_functions

--- super_net/loss_functions.py
+++ super_net/loss_functions.py
@@ -311,17 +311,16 @@ def make_chi2(make_data_values, make_pred_data):
     covmat = training_data.covmat
     central_values_idx = training_data.central_values_idx
 
+    # Invert the covmat
+    inv_covmat = jla.inv(covmat)
+
     @jax.jit
     def chi2(pdf):
         """ """
         diff = make_pred_data(pdf)[central_values_idx] - central_values
 
-        # decompose covmat after having batched it!
-        sqrt_covmat = jnp.array(sqrt_covmat_jax(covmat))
+        loss = jnp.einsum("i,ij,j", diff, inv_covmat, diff)
 
-        # solve_triangular: solve the equation a x = b for x, assuming a is a triangular matrix.
-        chi2_vec = jla.solve_triangular(sqrt_covmat, diff, lower=True)
-        loss = jnp.sum(chi2_vec**2)
         return loss
 
     return chi2
@@ -371,6 +370,9 @@ def make_chi2_with_positivity(
     covmat = training_data.covmat
     central_values_idx = training_data.central_values_idx
 
+    # Invert the covmat
+    inv_covmat = jla.inv(covmat)
+
     posdata_training_idx = make_posdata_split.training
 
     @jax.jit
@@ -378,70 +380,13 @@ def make_chi2_with_positivity(
         """ """
         diff = make_pred_data(pdf)[central_values_idx] - central_values
 
-        sqrt_covmat = jnp.array(sqrt_covmat_jax(covmat))
-
-        # solve_triangular: solve the equation a x = b for x, assuming a is a triangular matrix.
-        chi2_vec = jla.solve_triangular(sqrt_covmat, diff, lower=True)
-        loss = jnp.sum(chi2_vec**2)
+        loss = jnp.einsum("i,ij,j", diff, inv_covmat, diff)
 
         # add penalty term due to positivity
         pos_penalty = make_penalty_posdata(pdf, alpha, lambda_positivity)[
             posdata_training_idx
         ]
         loss += jnp.sum(pos_penalty)
-
-        return loss
-
-    return chi2
-
-
-def make_chi2_wmin_opt(make_data_values, make_pred_data, weight_minimization_grid):
-    """
-    Returns a jax.jit compiled function that computes the chi2
-    of a pdf grid optimised for weight minimisation.
-
-    Notes:
-        - Does not include positivity constraint.
-        - This function is designed for Bayesian like PDF fits.
-
-    Parameters
-    ----------
-    make_data_values: training_validation.MakeDataValues
-        dataclass containing data for training and validation.
-
-    make_pred_data: theory_predictions.make_pred_data
-        super_net provider for (fktable) theory predictions.
-
-    Returns
-    -------
-    @jax.jit Callable
-        function to compute chi2 of a pdf grid.
-
-    """
-    training_data = make_data_values.training_data
-    central_values = training_data.central_values
-    covmat = training_data.covmat
-    central_values_idx = training_data.central_values_idx
-
-    # Precompute predictions for the basis of wmin
-    predictions = jnp.array(
-        [
-            make_pred_data(weight_minimization_grid.wmin_INPUT_GRID[i])
-            for i in range(len(weight_minimization_grid.wmin_INPUT_GRID))
-        ]
-    )
-
-    # Invert the covmat
-    inv_covmat = jla.inv(covmat)
-
-    @jax.jit
-    def chi2(wmin_weights):
-        """ """
-        theory = jnp.einsum("i,ij", wmin_weights, predictions)
-
-        diff = theory[central_values_idx] - central_values
-
-        loss = jnp.einsum("i,ij,j", diff, inv_covmat, diff)
 
         return loss
 
