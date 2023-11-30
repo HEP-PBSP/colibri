1fa0a38669a13e36239f6364ecf654132c990e56
J-M-Moore <james.m.moore@aol.co.uk>
2023-11-30 14:20:58 +0000

    Added corner plot

--- super_net/wmin/wmin/wmin_plots.py
+++ super_net/wmin/wmin/wmin_plots.py
@@ -6,11 +6,14 @@ import sys
 import pathlib
 import os
 import json
+import corner
 
-from reportengine.figure import figuregen
+from reportengine.figure import figuregen, figure
 
 import matplotlib.pyplot as plt
 import numpy as np
+
+color_key = ['#66C2A5', '#FC8D62', '#8DA0CB']
 
 def get_fit_mc_weights_path(fit):
     fit_path = pathlib.Path(sys.prefix) / "share/super_net/results" / fit
@@ -37,13 +40,28 @@ def get_mc_fits_info(super_net_fits):
 
     return mc_weights
 
-@figuregen
-def plot_mc_weights_histograms(super_net_fits):
+def mc_weights(super_net_fits):
+    return get_mc_fits_info(super_net_fits)
+
+@figure
+def plot_mc_weights_corner_plot(mc_weights):
     """
     TODO
     """
 
-    mc_weights = get_mc_fits_info(super_net_fits)
+    fit_list = [fit for fit in mc_weights]
+    fig = corner.corner(mc_weights[fit_list[0]]['weights'].T, color=color_key[0], labels=[rf"$w_{i+1}$" for i in range(mc_weights[fit_list[0]]['nweights'])])
+    if len(fit_list) > 1:
+        for i, fit in enumerate(fit_list[1:]):
+            fig = corner.corner(mc_weights[fit]['weights'].T, fig=fig, color=color_key[i+1])
+    
+    return fig
+
+@figuregen
+def plot_mc_weights_histograms(mc_weights):
+    """
+    TODO
+    """
     max_weights = max([mc_weights[fit]['nweights'] for fit in mc_weights]) 
 
     for i in range(max_weights):
@@ -53,6 +71,10 @@ def plot_mc_weights_histograms(super_net_fits):
                ax.hist(mc_weights[fit]['weights'][i,:], label=fit, alpha=0.5)
            ax.set_ylabel("Frequency")
            ax.set_xlabel(rf"$w_{i+1}$")
+           x_left, x_right = ax.get_xlim()
+           y_low, y_high = ax.get_ylim()
+           #set aspect ratio
+           ax.set_aspect(abs((x_right-x_left)/(y_low-y_high)))
            ax.legend()
 
        yield fig
