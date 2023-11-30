91efca75dbe9988f520e66e80fe73e650dad7b7c
J-M-Moore <james.m.moore@aol.co.uk>
2023-11-30 20:55:39 +0000

    Fixed parametrisation information splash

--- super_net/wmin/wmin/wmin_plots.py
+++ super_net/wmin/wmin/wmin_plots.py
@@ -7,19 +7,90 @@ import pathlib
 import os
 import json
 import corner
+import yaml
 
 from reportengine.figure import figuregen, figure
+from validphys.core import PDF
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 color_key = ['#66C2A5', '#FC8D62', '#8DA0CB']
 
-def get_fit_mc_weights_path(fit):
+def print_parametrisations(super_net_fits):
+    """
+    Prints the parametric form that is used in each of the super_net_fits.
+    """
+
+    for fit in super_net_fits:
+        runcard = get_fit_runcard(fit)
+
+    if len(super_net_fits) == 1:
+        text = "This report displays the super_net fit performed in the parametrisation:\n\n"
+    else:
+        text = "This report compares super_net fits performed in the following " + str(len(super_net_fits)) + " parametrisations:\n\n"
+
+    for fit in super_net_fits:
+        runcard = get_fit_runcard(fit)
+        wminpdfset = runcard['wminpdfset']
+        n_replicas_wmin = runcard['n_replicas_wmin']
+        use_same_wmin_param_per_replica = runcard['use_same_wmin_param_per_replica']
+        if 'random_wmin_parametrisation' in runcard.keys():
+            random_wmin_parametrisation = runcard[random_wmin_parametrisation]
+        else:
+            random_wmin_parametrisation = False
+
+        text += "- " + fit + " has the parametrisation: " 
+
+        if not random_wmin_parametrisation:
+            if n_replicas_wmin == 2:
+                text += f"<div style=\"text-align:center\"> $\\textbf{{f}}(\\textbf{{w}}) = \\textbf{{f}}_0 + w_1 (\\textbf{{f}}_1 - \\textbf{{f}}_0) + w_{n_replicas_wmin} (\\textbf{{f}}_{n_replicas_wmin} - \\textbf{{f}}_0),$ </div>"
+
+            else:
+                text += f"<div style=\"text-align:center\"> $\\textbf{{f}}(\\textbf{{w}}) = \\textbf{{f}}_0 + w_1 (\\textbf{{f}}_1 - \\textbf{{f}}_0) + \cdots + w_{n_replicas_wmin} (\\textbf{{f}}_{n_replicas_wmin} - \\textbf{{f}}_0),$ </div>"
+
+            text += " where $\\textbf{f}_k$ is the $k$th replica drawn from the PDF set " + wminpdfset + " and $\\textbf{f}_0$ is the central replica of the same PDF set.\n\n"
+
+        else:
+            if use_same_wmin_param_per_replica:
+                if n_replicas_wmin == 2:
+                    text += f"<div style=\"text-align:center\"> $\\textbf{{f}}(\\textbf{{w}}) = \\textbf{f}_j + w_1 (\\textbf{{f}}_1 - \\textbf{{f}}_j) + w_{n_replicas_wmin} (\\textbf{{f}}_{n_replicas_wmin} - \\textbf{{f}}_j),$ </div>"
+
+                else:
+                    text += f"<div style=\"text-align:center\"> $\\textbf{{f}}(\\textbf{{w}}) = \\textbf{f}_j + w_1 (\\textbf{{f}}_1 - \\textbf{{f}}_j) + \cdots + w_{n_replicas_wmin} (\\textbf{{f}}_{n_replicas_wmin} - \\textbf{{f}}_j),$ </div>"
+
+                text += " where $\\textbf{f}_k$ is the $k$th replica drawn from the PDF set " + wminpdfset + " and $\\textbf{f}_j$ is a fixed replica, drawn at random from the same PDF set.\n\n"
+
+            else:
+                if n_replicas_wmin == 2:
+                    text += f"<div style=\"text-align:centre\"> $\\textbf{{f}}(\\\textbf{{w}}) = \\textbf{{f}}_{{j_i}} + w_1 (\\textbf{{f}}_1 - \\textbf{{f}}_{{j_i}}) + w_{n_replicas_wmin} (\\textbf{{f}}_{n_replcas_wmin} - \\textbf{{f}}_{{j_i}}),$ </div>"
+                else:
+                    text += f"<div style=\"text-align:centre\"> $\\textbf{{f}}(\\\textbf{{w}}) = \\textbf{{f}}_{{j_i}} + w_1 (\\textbf{{f}}_1 - \\textbf{{f}}_{{j_i}}) + \cdots + w_{n_replicas_wmin} (\\textbf{{f}}_{n_replcas_wmin} - \\textbf{{f}}_{{j_i}}),$ </div>"
+
+                text += " where $\\textbf{f}_k$ is the $k_j$th replica drawn from the PDF set " + wminpdfset + " and $\\textbf{f}_{j_i}$ is the $j_i$th replica drawn from the same PDF set randomly, for the $i$th Monte Carlo replica considered in the fit.\n\n"
+
+    return text
+
+def get_fit_path(fit):
     fit_path = pathlib.Path(sys.prefix) / "share/super_net/results" / fit
     if not os.path.exists(fit_path):
         raise FileNotFoundError("Could not find a fit " + fit + " in the super_net/results directory.")
 
+    return fit_path
+
+def get_fit_runcard(fit):
+    fit_path = get_fit_path(fit)
+    yaml_path = fit_path / "input/runcard.yaml"
+    if not os.path.exists(yaml_path):
+        raise FileNotFoundError("Could not find runcard for fit " + fit + " in the super_net/results directory.")
+
+    with open(yaml_path, 'r') as file:
+        fit_runcard = yaml.safe_load(file)
+
+    return fit_runcard
+
+def get_fit_mc_weights_path(fit):
+    fit_path = get_fit_path(fit)
     mc_weights_path = fit_path / "monte_carlo_results/monte_carlo_results.json"
     if not os.path.exists(mc_weights_path):
         raise FileNotFoundError("Could not find monte_carlo_results for " + fit + ".")
@@ -78,3 +149,18 @@ def plot_mc_weights_histograms(mc_weights):
            ax.legend()
 
        yield fig
+
+def plot_pdf_pulls(super_net_fits):
+    """
+    Plots the directions in which the PDFs pull for each of a collection of super_net_fits.
+    """
+
+    for fit in super_net_fits:
+        fit_data = get_fit_runcard(fit)
+
+        wminpdfset = fit_data['wminpdfset']
+        nweights = fit_data['n_replicas_wmin'] 
+
+        pdf = PDF(wminpdfset)
+#        print(pdf)
+#        print(dir(pdf))
