15a5a3d718a7c1b7b9e0b087dc5a5fdcec333c4f
J-M-Moore <james.m.moore@aol.co.uk>
2023-11-30 10:51:29 +0000

    Can now compare the weights of two wmin fits

--- super_net/wmin/wmin/wmin_plots.py
+++ super_net/wmin/wmin/wmin_plots.py
@@ -12,14 +12,7 @@ from reportengine.figure import figuregen
 import matplotlib.pyplot as plt
 import numpy as np
 
-@figuregen
-def plot_mc_weights_histograms(super_net_fit):
-    """
-    TODO
-    """
-
-    fit = super_net_fit    
-
+def get_fit_mc_weights_path(fit):
     fit_path = pathlib.Path(sys.prefix) / "share/super_net/results" / fit
     if not os.path.exists(fit_path):
         raise FileNotFoundError("Could not find a fit " + fit + " in the super_net/results directory.")
@@ -28,16 +21,38 @@ def plot_mc_weights_histograms(super_net_fit):
     if not os.path.exists(mc_weights_path):
         raise FileNotFoundError("Could not find monte_carlo_results for " + fit + ".")
 
-    with open(mc_weights_path) as f:
-        d = json.load(f)
+    return mc_weights_path
 
-    d = np.array(d)
-    d = d.T
+def get_mc_fits_info(super_net_fits):
+    mc_weights = {}
+    for fit in super_net_fits:
+        mc_weights_path = get_fit_mc_weights_path(fit)
 
-    for i, col in enumerate(d):
+        with open(mc_weights_path) as f:
+            d = json.load(f)
+
+        d = np.array(d)
+        nreps, nweights = d.shape
+        mc_weights[fit] = {'nweights': nweights, 'nreps': nreps, 'weights': d.T}
+
+    return mc_weights
+
+@figuregen
+def plot_mc_weights_histograms(super_net_fits):
+    """
+    TODO
+    """
+
+    mc_weights = get_mc_fits_info(super_net_fits)
+    max_weights = max([mc_weights[fit]['nweights'] for fit in mc_weights]) 
+
+    for i in range(max_weights):
        fig, ax = plt.subplots()
-       ax.hist(col)
-       ax.set_ylabel("Frequency")
-       ax.set_xlabel(rf"$w_{i+1}$")
+       for fit in mc_weights:
+           if i < mc_weights[fit]['nweights']:
+               ax.hist(mc_weights[fit]['weights'][i,:], label=fit, alpha=0.5)
+           ax.set_ylabel("Frequency")
+           ax.set_xlabel(rf"$w_{i+1}$")
+           ax.legend()
 
-       yield fig    
+       yield fig
