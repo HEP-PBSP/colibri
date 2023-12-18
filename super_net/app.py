7d415f15ceabdef04fd927206db8e0d3c9dd812a
J-M-Moore <james.m.moore@aol.co.uk>
2023-12-18 14:41:54 +0000

    Initial commit for LHAPDF grid writing in grid fit

--- super_net/app.py
+++ super_net/app.py
@@ -19,6 +19,7 @@ providers = [
     "super_net.closure_test.closure_test_estimators",
     "super_net.training_validation",
     "super_net.plots_and_tables.plotting",
+    "super_net.ns_utils",
 ]
 
 
--- super_net/grid_pdf/grid_pdf/app.py
+++ super_net/grid_pdf/grid_pdf/app.py
@@ -8,17 +8,41 @@ Date: 15.11.2023
 from super_net.app import SuperNetApp, providers
 from grid_pdf.config import GridPdfConfig
 
+import pathlib
+
 grid_pdf_providers = [
     *providers,
     "reportengine.report", 
     "grid_pdf.grid_pdf_model",
     "grid_pdf.grid_pdf_fit",
+    "grid_pdf.grid_pdf_lhapdf",
 ]
 
 
 class GridPdfApp(SuperNetApp):
     config_class = GridPdfConfig
 
+    @property
+    def argparser(self):
+        """Parser arguments for wmin app can be added here"""
+        parser = super().argparser
+
+        parser.add_argument(
+            "-o",
+            "--output",
+            nargs="?",
+            default=None,
+            help="Name of the output directory.",
+        )
+
+        return parser
+
+    def get_commandline_arguments(self, cmdline=None):
+        """Get commandline arguments"""
+        args = super().get_commandline_arguments(cmdline)
+        if args["output"] is None:
+            args["output"] = pathlib.Path(args["config_yml"]).stem
+        return args
 
 def main():
     a = GridPdfApp(name="grid_pdf", providers=grid_pdf_providers)
