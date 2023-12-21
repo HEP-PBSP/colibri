e7605efcc568ce652fe4b20bccaecc1a678453fb
Luca Mantani <luca.mantani@gmail.com>
2023-12-21 18:25:19 +0000

    Update super_net/grid_pdf/grid_pdf/grid_pdf_fit.py
    Co-authored-by: Mark Nestor Costantini <85164495+comane@users.noreply.github.com>

--- super_net/grid_pdf/grid_pdf/grid_pdf_fit.py
+++ super_net/grid_pdf/grid_pdf/grid_pdf_fit.py
@@ -9,11 +9,11 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def _bayesian_pdf_grid_fit(make__bayesian_pdf_grid_fit):
+def _bayesian_pdf_grid_fit(make_bayesian_pdf_grid_fit):
     """
-    Internal alias function for make__bayesian_pdf_grid_fit.
+    Internal alias function for make_bayesian_pdf_grid_fit.
     """
-    return make__bayesian_pdf_grid_fit
+    return make_bayesian_pdf_grid_fit
 
 
 def make_bayesian_pdf_grid_fit(
