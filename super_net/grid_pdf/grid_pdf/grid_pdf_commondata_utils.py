dd46f3f5b2e764b58e51ef51ca473ae0104db372
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-01-05 12:23:37 +0100

    interpolated grid closure test pdf

--- super_net/grid_pdf/grid_pdf/grid_pdf_commondata_utils.py
+++ super_net/grid_pdf/grid_pdf/grid_pdf_commondata_utils.py
@@ -1,13 +1,26 @@
+import jax.numpy as jnp
+
+from validphys import convolution
+
+from super_net.constants import XGRID
+from super_net.utils import FLAVOURS_ID_MAPPINGS
+
 from super_net.theory_predictions import make_pred_dataset
 
 
-def closure_test_pdf_grid_interpolated(closure_test_pdf, Q0=1.65):
+def closure_test_pdf_grid_interpolated(closure_test_pdf, xgrids, Q0=1.65):
     """
-    Computes the closure_test_pdf grid in the evolution basis.
+    Computes the closure_test_pdf grid in the evolution basis and
+
 
     Parameters
     ----------
     closure_test_pdf: validphys.core.PDF
+
+    xgrids: dict
+        defines the reduced grid, keys are flavour names and values are x values.
+        Each flavour needs to have a reduced grid assigned to in xgrids and
+        all flavours need to have the same number of x values.
 
     Q0: float, default is 1.65
 
@@ -17,11 +30,42 @@ def closure_test_pdf_grid_interpolated(closure_test_pdf, Q0=1.65):
         grid, is N_rep x N_fl x N_x
     """
 
-    'TODO'
+    # Every flavour (even unused ones like photon) needs to have a reduced grid assigned to in xgrids
+    # the flavour selection/mapping is then done by flavour_mapping (flavour_indices)
+
+    reduced_xgrid = jnp.concatenate(
+        [
+            convolution.evolution.grid_values(
+                closure_test_pdf, [fl], x_vals, [Q0]
+            ).squeeze(-1)
+            for fl, x_vals in xgrids.items()
+        ],
+        axis=1,
+    )
+
+    interpolated_xgrid = jnp.zeros((reduced_xgrid.shape[0], reduced_xgrid.shape[1], len(XGRID)))
+
+    for rep_idx in range(reduced_xgrid.shape[0]):
+
+        for fl_idx in range(reduced_xgrid.shape[1]):
+
+                interpolated_xgrid = interpolated_xgrid.at[rep_idx, fl_idx, :].set(
+                    jnp.interp(
+                        jnp.array(XGRID),
+                        jnp.array(xgrids[FLAVOURS_ID_MAPPINGS[fl_idx]]),
+                        reduced_xgrid[rep_idx, fl_idx, :],
+                    )
+                )
+    
+    return interpolated_xgrid 
+    
 
 
 def grid_pdf_closuretest_commondata_tuple(
-    data, experimental_commondata_tuple, closure_test_pdf_grid_interpolated, flavour_indices=None
+    data,
+    experimental_commondata_tuple,
+    closure_test_pdf_grid_interpolated,
+    flavour_indices=None,
 ):
     """
     returns a tuple (validphys nodes should be immutable)
