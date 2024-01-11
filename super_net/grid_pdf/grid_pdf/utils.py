363694cdd2f92b078483def9a67a775c961457ed
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-01-11 09:51:22 +0000

    accept empty flavour grids

--- super_net/grid_pdf/grid_pdf/utils.py
+++ super_net/grid_pdf/grid_pdf/utils.py
@@ -10,10 +10,12 @@ from super_net.constants import XGRID
 from super_net.utils import FLAVOURS_ID_MAPPINGS
 
 
-def closure_test_central_pdf_grid(closure_test_pdf, xgrids, Q0=1.65):
+def closure_test_central_pdf_grid(
+    closure_test_pdf, xgrids, length_reduced_xgrids, Q0=1.65
+):
     """
-    Computes the central member of the closure_test_pdf grid in the 
-    evolution basis and only on x points that are specified in xgrids. 
+    Computes the central member of the closure_test_pdf grid in the
+    evolution basis and only on x points that are specified in xgrids.
     The grid is then interpolated to the full XGRID.
 
     NOTE: this function overrides the one in super_net.utils
@@ -24,8 +26,8 @@ def closure_test_central_pdf_grid(closure_test_pdf, xgrids, Q0=1.65):
 
     xgrids: dict
         defines the reduced grid, keys are flavour names and values are x values.
-        Each flavour needs to have a reduced grid assigned to in xgrids and
-        all flavours need to have the same number of x values.
+        All flavours need to have the same number of x values.
+        Flavours with no x values are assigned a zero grid.
 
     Q0: float, default is 1.65
 
@@ -34,24 +36,24 @@ def closure_test_central_pdf_grid(closure_test_pdf, xgrids, Q0=1.65):
     grid: jnp.array
         grid, is N_fl x N_x
     """
+    # fill with zeros the xgrids that are not used
+    xgrids = {
+        fl: jnp.array(x_vals) if x_vals else jnp.zeros(length_reduced_xgrids)
+        for fl, x_vals in xgrids.items()
+    }
 
-    # Every flavour (even unused ones like photon) needs to have a reduced grid assigned to in xgrids,
     # the flavour selection/mapping is then done by flavour_mapping (flavour_indices)
-
     reduced_xgrid = jnp.array(
         [
-            convolution.evolution.grid_values(
-                closure_test_pdf, [fl], x_vals, [Q0]
-            ).squeeze(-1)[0].squeeze(0)
+            convolution.evolution.grid_values(closure_test_pdf, [fl], x_vals, [Q0])
+            .squeeze(-1)[0]
+            .squeeze(0)
             for fl, x_vals in xgrids.items()
         ],
     )
-    
-    interpolated_xgrid = jnp.zeros(
-        (reduced_xgrid.shape[0], len(XGRID))
-    )
 
-    
+    interpolated_xgrid = jnp.zeros((reduced_xgrid.shape[0], len(XGRID)))
+
     for fl_idx in range(reduced_xgrid.shape[1]):
         interpolated_xgrid = interpolated_xgrid.at[fl_idx, :].set(
             jnp.interp(
@@ -60,5 +62,5 @@ def closure_test_central_pdf_grid(closure_test_pdf, xgrids, Q0=1.65):
                 reduced_xgrid[fl_idx, :],
             )
         )
-    
+
     return interpolated_xgrid
