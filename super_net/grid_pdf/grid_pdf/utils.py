fba4fce84080a4dd9eba7880d3d68fce9cec93ae
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-01-11 10:07:31 +0000

    allow for not overriding

--- super_net/grid_pdf/grid_pdf/utils.py
+++ super_net/grid_pdf/grid_pdf/utils.py
@@ -6,19 +6,21 @@ import jax.numpy as jnp
 
 from validphys import convolution
 
+import super_net
 from super_net.constants import XGRID
 from super_net.utils import FLAVOURS_ID_MAPPINGS
 
 
 def closure_test_central_pdf_grid(
-    closure_test_pdf, xgrids, length_reduced_xgrids, Q0=1.65
+    closure_test_pdf, xgrids, length_reduced_xgrids, reduced_xgrid_data=True, Q0=1.65
 ):
     """
     Computes the central member of the closure_test_pdf grid in the
     evolution basis and only on x points that are specified in xgrids.
     The grid is then interpolated to the full XGRID.
 
-    NOTE: this function overrides the one in super_net.utils
+    NOTE: when reduced_xgrid_data=True, this function overrides the one in super_net.utils
+    otherwise the one in super_net.utils is used.
 
     Parameters
     ----------
@@ -29,6 +31,13 @@ def closure_test_central_pdf_grid(
         All flavours need to have the same number of x values.
         Flavours with no x values are assigned a zero grid.
 
+    length_reduced_xgrids: int
+        lenght of the reduced xgrids
+    
+    reduced_xgrid_data: bool, default is True
+        When True the closure_test_central_pdf_grid is overriden.
+        When False the closure_test_pdf_grid from super_net.utils is used.
+    
     Q0: float, default is 1.65
 
     Returns
@@ -36,6 +45,10 @@ def closure_test_central_pdf_grid(
     grid: jnp.array
         grid, is N_fl x N_x
     """
+    
+    if not reduced_xgrid_data:
+        return super_net.utils.closure_test_pdf_grid(closure_test_pdf, Q0=Q0)[0]
+    
     # fill with zeros the xgrids that are not used
     xgrids = {
         fl: jnp.array(x_vals) if x_vals else jnp.zeros(length_reduced_xgrids)
