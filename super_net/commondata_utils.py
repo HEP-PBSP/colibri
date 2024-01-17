Fixed commondata_tuple production for level 0, level 1 pseudodata and added new covariance matrices

super_net/commondata_utils.py
@@ -36,7 +36,7 @@ def experimental_commondata_tuple(data):
     return tuple(data.load_commondata_instance())
 
 
-def closuretest_commondata_tuple(
+def level_0_commondata_tuple(
     data,
     experimental_commondata_tuple,
     closure_test_central_pdf_grid,
@@ -79,74 +79,54 @@ def closuretest_commondata_tuple(
     return tuple(fake_data)
 
 
-def pseudodata_commondata_tuple(data, experimental_commondata_tuple, replica_seed):
+def level_1_commondata_tuple(
+    level_0_commondata_tuple,
+    data_generation_covariance_matrix,
+    level_1_seed=123456,
+):
     """
-    Returns a tuple (validphys nodes should be immutable)
+    returns a tuple (validphys nodes should be immutable)
     of commondata instances with experimental central values
-    fluctuated with random noise sampled from experimental
-    covariance matrix
+    replaced with theory predictions computed from a PDF `closure_test_pdf`
+    and fktables corresponding to datasets within data
+
+    Noise is added on top of the central values according to a
+    multivariate Gaussian with covariance data_generation_covariance_matrix
 
     Parameters
     ----------
-    data: super_net.core.SuperNetDataGroupSpec
+    level_0_commondata_tuple: tuple of validphys.coredata.CommonData instances
+        A tuple of level_0 closure test data.
 
-    experimental_commondata_tuple: tuple
-        tuple of commondata with experimental central values
+    data_generation_covariance_matrix: jnp.array
+        The covariance matrix used for data generation.
 
-    replica_seed: int
-        seed used for the sampling of random noise
-
-    Returns
-    -------
-    tuple
-        tuple of validphys.coredata.CommonData instances
-    """
-
-    index = data.data_index()
-    dataset_order = [cd.setname for cd in experimental_commondata_tuple]
-    pseudodata_list = make_level1_data(
-        data, experimental_commondata_tuple, replica_seed, index, sep_mult=True
-    )
-    pseudodata_list = sorted(
-        pseudodata_list, key=lambda obj: dataset_order.index(obj.setname)
-    )
-    return tuple(pseudodata_list)
-
-
-def closuretest_pseudodata_commondata_tuple(
-    data, closuretest_commondata_tuple, replica_seed
-):
-    """
-    Like `pseudodata_commondata_tuple` but with closure test (fake-data) central values.
+    level_1_seed: int
+        The random seed from which the level_1 data is drawn.
 
     Returns
     -------
     tuple
         tuple of validphys.coredata.CommonData instances
     """
-    return pseudodata_commondata_tuple(data, closuretest_commondata_tuple, replica_seed)
 
+    # First, construct a jax array from the level_0_commondata_tuple
+    central_values = jnp.array(pd.concat([cd.central_values for cd in level_0_commondata_tuple], axis=0))
 
-"""
-Collect over multiple random seeds so as to generate multiple commondata instances.
-To be used in a Monte Carlo fit to experimental data.
-"""
-mc_replicas_pseudodata_commondata_tuple = collect(
-    "pseudodata_commondata_tuple", ("replica_indices",)
-)
+    # Now, sample from the multivariate Gaussian with central values central_values
+    # and covariance matrix data_generation_covariance_matrix. This produces the
+    # level_1 data.
+    rng = jax.random.PRNGKey(level_1_seed)
+    sample = jax.random.multivariate_normal(rng, central_values, data_generation_covariance_matrix)
 
-"""
-Collect over multiple random seeds so as to generate multiple commondata instances.
-To be used in a Monte Carlo closure test fit.
-"""
-mc_replicas_closuretest_pseudodata_commondata_tuple = collect(
-    "closuretest_pseudodata_commondata_tuple",
-    ("replica_indices",),
-)
+    # Now, reconstruct the commondata tuple, by modifying the original commondata
+    # tuple's central values.
+    sample_list = []
+    for cd in level_0_commondata_tuple:
+        sample_list.append(cd.with_central_value(sample[:cd.ndata]))
+        sample = sample[cd.ndata:]
 
-"""
-validphys actions should return immutable objects
-"""
+    return tuple(pseudodata)
 
 
 @dataclass(frozen=True)
@@ -158,8 +138,7 @@ class CentralCovmatIndex:
     def to_dict(self):
         return asdict(self)
 
-
-def central_covmat_index(commondata_tuple, covariance_matrix):
+def central_covmat_index(commondata_tuple, fit_covariance_matrix):
     """
     Given a commondata_tuple and a covariance_matrix, generated
     according to respective explicit node in config.py, store
@@ -172,10 +151,10 @@ def central_covmat_index(commondata_tuple, covariance_matrix):
         (see config.produce_commondata_tuple) and accordingly to the
         specified options (pseudodata, fakedata).
 
-    covariance_matrix: jnp.array
+    fit_covariance_matrix: jnp.array
         covariance matrix, is generated as explicit node
-        (see config.covariance_matrix) can be either experimental
-        or t0 covariance matrix depending on whether `use_t0` is
+        (see config.fit_covariance_matrix) can be either experimental
+        or t0 covariance matrix depending on whether `use_fit_t0` is
         True or False
 
     Returns
@@ -192,11 +171,11 @@ def central_covmat_index(commondata_tuple, covariance_matrix):
     return CentralCovmatIndex(
         central_values=central_values,
         central_values_idx=central_values_idx,
-        covmat=covariance_matrix,
+        covmat=fit_covariance_matrix,
     )
 
-
-"""
-Collect central_covmat_index over multiple replicas
-"""
-mc_central_covmat_index = collect("central_covmat_index", ("replica_indices",))
+def pseudodata_central_covmat_index(commondata_tuple, data_generation_covariance_matrix):
+    """Same as central_covmat_index, but with the pseudodata generation
+    covariance matrix for a Monte Carlo fit.
+    """
+    return central_covmat_index(commondata_tuple, data_generation_covariance_matrix)