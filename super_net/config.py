f94e64142c2bdf2b42a216f92310fa0c045c8350
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-01-19 16:11:49 +0000

    vectorized production rule

--- super_net/config.py
+++ super_net/config.py
@@ -110,7 +110,11 @@ class SuperNetConfig(Config):
             ns_settings["resume"] = "overwrite"
 
         return ns_settings
-
+    
+    def produce_vectorized(self, ns_settings):
+        """Returns True if the fit is vectorized, False otherwise."""
+        return ns_settings["vectorized"]
+    
     @explicit_node
     def produce_commondata_tuple(self, closure_test_level=False):
         """
