9ab42814748a1e965c52ad736bf43f9b05056948
Luca Mantani <luca.mantani@gmail.com>
2024-02-08 22:06:51 +0000

    Update dependencies in setup_conda_env.sh

--- .github/setup_conda_env.sh
+++ .github/setup_conda_env.sh
@@ -4,5 +4,5 @@ source $(conda info --base)/etc/profile.d/conda.sh && conda activate test 2>/dev
 conda install nnpdf -y
 conda install mpi4py -y
 python -m pip install --upgrade pip
-python -m pip install flake8 pytest ultranest jax optax flax flit
+python -m pip install flake8 pytest ultranest jax==0.4.13 optax==0.1.7 flax flit
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
