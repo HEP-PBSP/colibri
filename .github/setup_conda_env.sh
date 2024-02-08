b7afc3c8da12e4c769465bc9b15a5509d11f558a
Luca Mantani <luca.mantani@gmail.com>
2024-02-08 21:12:18 +0000

    Use script to create the environment

---
+++ .github/setup_conda_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+conda activate test
+conda install nnpdf -y
+conda install mpi4py -y
+python -m pip install --upgrade pip
+python -m pip install flake8 pytest ultranest jax optax flax flit
+if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
