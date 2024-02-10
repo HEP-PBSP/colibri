798974245fc80cd166ae176c95754062fdb4cb28
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-02-10 14:22:31 +0000

    added config

---
+++ colibri/models/nnpdf_colibri/nnpdf_colibri/config.py
@@ -0,0 +1,20 @@
+"""
+nnpdf_colibri.config.py
+
+Config module of nnpdf_colibri
+
+"""
+
+from colibri.config import colibriConfig, Environment
+
+
+class Environment(Environment):
+    pass
+
+
+class NNPDFColibriConfig(colibriConfig):
+    """
+    GridConfig class Inherits from colibri.config.colibriConfig
+    """
+
+    pass
