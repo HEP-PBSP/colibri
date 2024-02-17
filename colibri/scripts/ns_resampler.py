6787dbebf319d4e7e5d7d17b0728e6afca2f8c6a
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-02-17 18:58:23 +0000

    rank 0 for the rest

--- colibri/scripts/ns_resampler.py
+++ colibri/scripts/ns_resampler.py
@@ -23,6 +23,11 @@ log.setLevel(logging.INFO)
 log.addHandler(colors.ColorHandler())
 
 
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+
 def main():
     parser = argparse.ArgumentParser(description="Script to resample from NS posterior")
     parser.add_argument("fit_name", help="The colibri fit from which to sample.")
@@ -50,72 +55,72 @@ def main():
     )
 
     args = parser.parse_args()
-    if args.resampled_fit_name is None:
-        args.resampled_fit_name = "resampled_" + args.fit_name
 
-    # Convert fit_path to a pathlib.Path object
-    fit_path = pathlib.Path(args.fit_name)
+    if rank == 0:
+            
+        if args.resampled_fit_name is None:
+            args.resampled_fit_name = "resampled_" + args.fit_name
 
-    # path of resampled fit
-    resampled_fit_path = pathlib.Path(args.resampled_fit_name)
+        # Convert fit_path to a pathlib.Path object
+        fit_path = pathlib.Path(args.fit_name)
 
-    # Give names to other arguments
-    nreplicas = args.nreplicas
-    resampling_seed = args.resampling_seed
+        # path of resampled fit
+        resampled_fit_path = pathlib.Path(args.resampled_fit_name)
 
-    # check whether fit path exists
+        # Give names to other arguments
+        nreplicas = args.nreplicas
+        resampling_seed = args.resampling_seed
 
-    log.info(f"Loading pdf model from {fit_path}")
-    # load pdf_model from fit using dill
-    with open(fit_path / "pdf_model.pkl", "rb") as file:
-        pdf_model = dill.load(file)
+        # check whether fit path exists
 
-    # Check that the .txt file with posterior samples exists
-    if not os.path.exists(fit_path / "ultranest_logs/chains/equal_weighted_post.txt"):
-        raise FileNotFoundError(
-            f"{fit_path}/ultranest_logs/chains/equal_weighted_post.txt does not exist;"
-            "please run the bayesian fit first."
+        log.info(f"Loading pdf model from {fit_path}")
+        # load pdf_model from fit using dill
+        with open(fit_path / "pdf_model.pkl", "rb") as file:
+            pdf_model = dill.load(file)
+
+        # Check that the .txt file with posterior samples exists
+        if not os.path.exists(fit_path / "ultranest_logs/chains/equal_weighted_post.txt"):
+            raise FileNotFoundError(
+                f"{fit_path}/ultranest_logs/chains/equal_weighted_post.txt does not exist;"
+                "please run the bayesian fit first."
+            )
+
+        equal_weight_post_path = fit_path / "ultranest_logs/chains/equal_weighted_post.txt"
+
+        samples = pd.read_csv(equal_weight_post_path, sep="\s+", dtype=float).values
+
+        if nreplicas > samples.shape[0]:
+            nreplicas = samples.shape[0]
+            log.warning(
+                f"The chosen number of posterior samples exceeds the number of posterior"
+                "samples computed by ultranest. Setting the number of resampled posterior"
+                f"samples to {nreplicas}"
+            )
+
+        resampled_posterior = resample_from_ns_posterior(
+            samples,
+            nreplicas,
+            resampling_seed,
         )
 
-    equal_weight_post_path = fit_path / "ultranest_logs/chains/equal_weighted_post.txt"
+        # copy old fit to resampled fit
+        os.system(f"cp -r {fit_path} {resampled_fit_path}")
 
-    samples = pd.read_csv(equal_weight_post_path, sep="\s+", dtype=float).values
+        # remove old replicas from resampled fit
+        os.system(f"rm -r {resampled_fit_path}/replicas")
 
-    if nreplicas > samples.shape[0]:
-        nreplicas = samples.shape[0]
-        log.warning(
-            f"The chosen number of posterior samples exceeds the number of posterior"
-            "samples computed by ultranest. Setting the number of resampled posterior"
-            f"samples to {nreplicas}"
-        )
+        # overwrite old ns_result.csv with resampled posterior
+        parameters = pdf_model.param_names
+        df = pd.DataFrame(resampled_posterior, columns=parameters)
+        df.to_csv(str(resampled_fit_path) + "/ns_result.csv")
 
-    resampled_posterior = resample_from_ns_posterior(
-        samples,
-        nreplicas,
-        resampling_seed,
-    )
+    comm.Barrier()
 
-    # copy old fit to resampled fit
-    os.system(f"cp -r {fit_path} {resampled_fit_path}")
+    # Distribute indices among processes using scatter
+    indices_per_process = list(range(rank, nreplicas, size))
 
-    # remove old replicas from resampled fit
-    os.system(f"rm -r {resampled_fit_path}/replicas")
-
-    # overwrite old ns_result.csv with resampled posterior
-    parameters = pdf_model.param_names
-    df = pd.DataFrame(resampled_posterior, columns=parameters)
-    df.to_csv(str(resampled_fit_path) + "/ns_result.csv")
-
-    # Initialize MPI communication
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    size = comm.Get_size()
-
-    # Distribute exportgrid writing tasks across processes
-    start_index = rank * resampled_posterior.shape[0] // size
-    end_index = (rank + 1) * resampled_posterior.shape[0] // size
-
-    for i in range(start_index, end_index):
+    # Finish by writing the replicas to export grids, ready for evolution
+    for i in indices_per_process:
         log.info(f"Writing exportgrid for replica {i+1} on rank {rank}")
         write_exportgrid(
             resampled_posterior[i],
@@ -124,4 +129,7 @@ def main():
             resampled_fit_path,
         )
 
-    log.info(f"Resampling completed. Resampled fit stored in {resampled_fit_path}")
+    # Synchronize to ensure all processes have finished
+    comm.Barrier()
+    if rank == 0:
+        log.info(f"Resampling completed. Resampled fit stored in {resampled_fit_path}")
