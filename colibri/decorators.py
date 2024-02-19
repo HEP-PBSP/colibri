48cfb0292a6df7b63d9be404ae1522c90bb60fd5
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-02-19 10:24:33 +0000

    format

--- colibri/decorators.py
+++ colibri/decorators.py
@@ -1,13 +1,24 @@
+"""
+colibri.decorators
+
+This module contains decorators that can be used to modify the behaviour of functions.
+"""
+
 import functools
 
 import jax
 
 
 def enable_x64(func):
+    """
+    Enable 64-bit precision for the duration of the function call.
+    """
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         jax.config.update("jax_enable_x64", True)
         result = func(*args, **kwargs)
         jax.config.update("jax_enable_x64", False)
         return result
+
     return wrapper
