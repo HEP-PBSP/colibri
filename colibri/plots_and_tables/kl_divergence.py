54ffcc9fcd826821dc9aca81b65cd988ac82ef29
Luca Mantani <luca.mantani@gmail.com>
2024-03-06 19:38:16 +0000

    Added resample plot

--- colibri/plots_and_tables/kl_divergence.py
+++ colibri/plots_and_tables/kl_divergence.py
@@ -7,7 +7,7 @@ from colibri.plots_and_tables.plotting import get_fit_path
 from scipy.stats import percentileofscore
 
 
-def gaussian_kl_divergence(x, y):
+def gaussian_kl_divergence(x, y, symm=False):
     # Compute mean and covariance matrix
     mean_x = np.mean(x, axis=0)
     mean_y = np.mean(y, axis=0)
@@ -29,13 +29,21 @@ def gaussian_kl_divergence(x, y):
         + (mean_y - mean_x) @ np.linalg.inv(cov_y) @ (mean_y - mean_x)
     )
 
+    if symm:
+        kl_div += 0.5 * (
+            np.log(np.linalg.det(cov_x) / np.linalg.det(cov_y))
+            - len(mean_x)
+            + np.trace(np.linalg.inv(cov_x) @ cov_y)
+            + (mean_x - mean_y) @ np.linalg.inv(cov_x) @ (mean_x - mean_y)
+        )
+
     if len(x.shape) == 1:
         kl_div = kl_div[0, 0]
 
     return kl_div
 
 
-def kl_div_test(fit_A, fit_B, n_permutations=1000):
+def kl_div_test_resample(fit_A, fit_B, n_permutations=1000, symm=False, n_resample=100):
     """
     TODO
     """
@@ -50,13 +58,46 @@ def kl_div_test(fit_A, fit_B, n_permutations=1000):
     x_A = df_A.values
     x_B = df_B.values
 
-    kl_value = gaussian_kl_divergence(x_A, x_B)
+    kl_distro = []
+    for i in range(n_permutations):
+        x_A_aux = x_A[np.random.choice(x_A.shape[0], n_resample, replace=False)]
+        x_B_aux = x_B[np.random.choice(x_B.shape[0], n_resample, replace=False)]
+        kl_value = gaussian_kl_divergence(x_A_aux, x_B_aux, symm=symm)
+        kl_distro.append(kl_value)
+        # print(f"KL divergence between fit A and fit B: {kl_value}")
+
+    kl_values_perm = []
+    for i in range(n_permutations):
+        perm_x, perm_y = permute_x_y_samples(
+            x_A[:n_resample], x_B[:n_resample], random_seed=i
+        )
+        kl_values_perm.append(gaussian_kl_divergence(perm_x, perm_y, symm=symm))
+
+    return {"kl_distribution": kl_values_perm, "kl_distro": kl_distro}
+
+
+def kl_div_test(fit_A, fit_B, n_permutations=1000, symm=False):
+    """
+    TODO
+    """
+    fit_A_path = get_fit_path(fit_A)
+    fit_B_path = get_fit_path(fit_B)
+
+    # Each folder has only one result file
+    # Read the result file, no matter the type of fit
+    df_A = pd.read_csv(glob.glob(fit_A_path + "/*_result.csv")[0], index_col=0)
+    df_B = pd.read_csv(glob.glob(fit_B_path + "/*_result.csv")[0], index_col=0)
+
+    x_A = df_A.values
+    x_B = df_B.values
+
+    kl_value = gaussian_kl_divergence(x_A, x_B, symm=symm)
     print(f"KL divergence between fit A and fit B: {kl_value}")
 
     kl_values_perm = []
     for i in range(n_permutations):
         perm_x, perm_y = permute_x_y_samples(x_A, x_B, random_seed=i)
-        kl_values_perm.append(gaussian_kl_divergence(perm_x, perm_y))
+        kl_values_perm.append(gaussian_kl_divergence(perm_x, perm_y, symm=symm))
 
     return {"kl_distribution": kl_values_perm, "kl_value": kl_value}
 
@@ -115,6 +156,38 @@ def permute_x_y_samples(x, y, random_seed=0):
     perm_y = permute_x_y[x.shape[0] :, :]
 
     return perm_x, perm_y
+
+
+@figure
+def plot_kl_distribution_resample(kl_div_test_resample, title=""):
+    """
+    TODO
+    """
+    kl_distribution = kl_div_test_resample["kl_distribution"]
+    kl_distro = kl_div_test_resample["kl_distro"]
+
+    fig, ax = plt.subplots()
+    ax.hist(
+        kl_distribution,
+        bins=20,
+        color="blue",
+        alpha=0.7,
+        label="Permutation distribution",
+        density=True,
+    )
+
+    ax.hist(
+        kl_distro,
+        bins=20,
+        color="red",
+        alpha=0.7,
+        label="",
+        density=True,
+    )
+    ax.set_xlabel("KL divergence")
+    ax.set_ylabel("Frequency")
+
+    return fig
 
 
 @figure
