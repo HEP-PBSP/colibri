2df567874b66c9bfce1ecd203f1576da89e07751
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-04-18 14:26:44 +0100

    get FIT_XGRID from production rule using the colibri API

--- colibri/tests/test_XGRID.py
+++ colibri/tests/test_XGRID.py
@@ -8,7 +8,6 @@ from colibri.api import API as cAPI
 from colibri.constants import XGRID
 from colibri.tests.conftest import TEST_FULL_DIS_DATASET, TEST_FULL_HAD_DATASET
 from numpy.testing import assert_allclose
-from validphys.fkparser import load_fktable
 
 
 def test_dis_xgrid():
@@ -17,18 +16,9 @@ def test_dis_xgrid():
     in conftest is the same as the XGRID stored in constants.py
     """
 
-    data = cAPI.data(**TEST_FULL_DIS_DATASET)
+    FIT_XGRID = cAPI.FIT_XGRID(**TEST_FULL_DIS_DATASET)
 
-    xgrids = set()
-
-    for ds in data.datasets:
-        for fkspec in ds.fkspecs:
-            fktable = load_fktable(fkspec)
-            xgrids.update(fktable.xgrid)
-
-    xgrid_union = np.array(sorted(xgrids))
-
-    assert_allclose(xgrid_union, np.array(XGRID))
+    assert_allclose(FIT_XGRID, np.array(XGRID))
 
 
 def test_had_xgrid():
@@ -37,16 +27,7 @@ def test_had_xgrid():
     in conftest is the same as the XGRID stored in constants.py
     """
 
-    data = cAPI.data(**TEST_FULL_HAD_DATASET)
-
-    xgrids = set()
-
-    for ds in data.datasets:
-        for fkspec in ds.fkspecs:
-            fktable = load_fktable(fkspec)
-            xgrids.update(fktable.xgrid)
-
-    xgrid_union = np.array(sorted(xgrids))
+    FIT_XGRID = cAPI.FIT_XGRID(**TEST_FULL_HAD_DATASET)
 
     # note: the very low x values are not used for the hadronic datasets
-    assert_allclose(xgrid_union, np.array(XGRID[6:]))
+    assert_allclose(FIT_XGRID, np.array(XGRID[6:]))
