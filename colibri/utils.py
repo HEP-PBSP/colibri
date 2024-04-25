8abaf20e67155e424ca7ad6d217cc9e35aabea11
Mark Nestor Costantini <85164495+comane@users.noreply.github.com>
2024-04-25 14:07:15 +0100

    Update colibri/utils.py
    Co-authored-by: Luca Mantani <luca.mantani@gmail.com>

--- colibri/utils.py
+++ colibri/utils.py
@@ -47,7 +47,7 @@ def fill_dis_fkarr_with_zeros(fktable, FIT_XGRID):
 def fill_had_fkarr_with_zeros(fktable, FIT_XGRID):
     """
     Fills the FK array with zeros so as to get array of shape
-    (Ndat, Nfl, 50, 50)
+    (Ndat, Nfl, N_FIT_XGRID, N_FIT_XGRID)
 
     Parameters
     ----------
