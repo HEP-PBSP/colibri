Fixed calculation of evidence and added various logs

colibri/analytic_fit.py
@@ -109,10 +109,37 @@ def analytic_fit(
     sol_mean = jla.inv(X.T @ Sigma @ X) @ X.T @ Sigma @ Y
     sol_covmat = jla.inv(X.T @ Sigma @ X)
 
-    # Compute evidence logZ
-    extra = -0.5 * (Y @ Sigma @ Y - Y @ Sigma @ X @ sol_mean)
-    logZ = jnp.log(jnp.sqrt(jla.det(2 * jnp.pi * sol_covmat))) + extra
-    log.info(f"LogZ = {logZ}")
+    if analytic_settings["compute_evidence"]:
+        # Compute the evidence
+        # This is the log of the evidence, which is the log of the integral of the likelihood
+        # over the prior. The prior is a gaussian with width prior_width.
+        log.info("Computing the evidence...")
+        log.warning("Supporting only cubic priors.")
+        log.warning(
+            "Assuming that the prior is wide enough to fully cover the gaussian likelihood."
+        )
+
+        # Check if prior width is set, if not set to 1.0
+        if "prior_width" not in analytic_settings:
+            prior_width = 1.0
+            log.warning(
+                "No prior width specified, setting it to 1.0. This might not be desired."
+            )
+        else:
+            prior_width = analytic_settings["prior_width"]
+
+        gaussian_integral = jnp.log(jnp.sqrt(jla.det(2 * jnp.pi * sol_covmat)))
+        log_prior = len(sol_mean) * jnp.log(1 / prior_width)
+        # This is a factor in front of the gaussian likelihood
+        extra_term = -0.5 * (Y @ Sigma @ Y - Y @ Sigma @ X @ sol_mean)
+
+        logZ = gaussian_integral + extra_term + log_prior
+
+        log.info(f"LogZ = {logZ}")
+
+        # Write the evidence to file
+        with open(str(output_path) + "/evidence.csv", "w") as f:
+            f.write(f"LogZ\n{logZ}")
 
     key = jax.random.PRNGKey(analytic_settings["sampling_seed"])
 