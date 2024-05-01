3d247c881823f0330ee8eb26962cd7e40dec9bf0
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-05-01 17:31:21 +0200

    added test module for utils

--- colibri/tests/test_utils.py
+++ colibri/tests/test_utils.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from colibri.utils import cast_to_numpy
 
+
 def test_cast_to_numpy():
     """
     test the cast_to_numpy function
@@ -16,10 +17,7 @@ def test_cast_to_numpy():
     @jax.jit
     def test_func(x):
         return x
-    
+
     x = jax.numpy.array([1, 2, 3])
-    
+
     assert type(test_func(x)) == np.ndarray
-
-    
-
