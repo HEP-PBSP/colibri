16fe576a3b9fea1c503b86d0c9a0f3233f324e70
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-05-17 14:54:30 +0100

    removed unaccessed imports

--- colibri/checks.py
+++ colibri/checks.py
@@ -1,5 +1,6 @@
-from colibri.utils import pdf_models_equal, get_pdf_model
-from reportengine.checks import make_argcheck, check
+from reportengine.checks import make_argcheck
+
+from colibri.utils import get_pdf_model, pdf_models_equal
 
 
 @make_argcheck
@@ -9,12 +10,13 @@ def check_pdf_models_equal(prior_settings, pdf_model):
     PDF model used as prior (eg when using prior_settings["type"] == "prior_from_gauss_posterior")
     matches the PDF model used in the current fit (pdf_model).
     """
-    
+
     if prior_settings["type"] == "prior_from_gauss_posterior":
-        
+
         prior_fit = prior_settings["prior_fit"]
         prior_pdf_model = get_pdf_model(prior_fit)
 
         if not pdf_models_equal(prior_pdf_model, pdf_model):
-            raise ValueError(f"PDF model {pdf_model} does not match prior settings {prior_pdf_model}")
-
+            raise ValueError(
+                f"PDF model {pdf_model} does not match prior settings {prior_pdf_model}"
+            )
