f5f875dbf08ddaa5e022659f99d47355213629c6
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-06-05 11:40:08 +0100

    changed monte_carlo_fit to run_monte_carlo_fit

--- colibri/monte_carlo_fit.py
+++ colibri/monte_carlo_fit.py
@@ -33,18 +33,23 @@ class MonteCarloFit:
         Array containing the training loss.
     validation_loss: jnp.array
         Array containing the validation loss.
+    optimized_parameters: jnp.array
+        Array containing the optimized parameters.
 
     """
 
     monte_carlo_specs: dict
     training_loss: jnp.array
     validation_loss: jnp.array
+    optimized_parameters: jnp.array
 
 
 def monte_carlo_fit(
     _chi2_training_data_with_positivity,
     _chi2_validation_data_with_positivity,
     _pred_data,
+    fast_kernel_arrays,
+    positivity_fast_kernel_arrays,
     len_trval_data,
     pdf_model,
     mc_initial_parameters,
@@ -126,24 +131,57 @@ def monte_carlo_fit(
     pred_and_pdf = pdf_model.pred_and_pdf_func(FIT_XGRID, forward_map=_pred_data)
 
     @jax.jit
-    def loss_training(parameters, batch_idx):
-        predictions, pdf = pred_and_pdf(parameters)
+    def loss_training(
+        parameters,
+        batch_idx,
+        fast_kernel_arrays,
+        positivity_fast_kernel_arrays,
+        alpha,
+        lambda_positivity,
+    ):
+        predictions, pdf = pred_and_pdf(parameters, fast_kernel_arrays)
 
         return _chi2_training_data_with_positivity(
-            predictions, pdf, batch_idx, alpha, lambda_positivity
+            predictions,
+            pdf,
+            batch_idx,
+            alpha,
+            lambda_positivity,
+            positivity_fast_kernel_arrays,
         )
 
     @jax.jit
-    def loss_validation(parameters):
-        predictions, pdf = pred_and_pdf(parameters)
+    def loss_validation(
+        parameters,
+        fast_kernel_arrays,
+        positivity_fast_kernel_arrays,
+        alpha,
+        lambda_positivity,
+    ):
+        predictions, pdf = pred_and_pdf(parameters, fast_kernel_arrays)
 
         return _chi2_validation_data_with_positivity(
-            predictions, pdf, alpha, lambda_positivity
+            predictions, pdf, alpha, lambda_positivity, positivity_fast_kernel_arrays
         )
 
     @jax.jit
-    def step(params, opt_state, batch_idx):
-        loss_value, grads = jax.value_and_grad(loss_training)(params, batch_idx)
+    def step(
+        params,
+        opt_state,
+        batch_idx,
+        fast_kernel_arrays,
+        positivity_fast_kernel_arrays,
+        alpha,
+        lambda_positivity,
+    ):
+        loss_value, grads = jax.value_and_grad(loss_training)(
+            params,
+            batch_idx,
+            fast_kernel_arrays,
+            positivity_fast_kernel_arrays,
+            alpha,
+            lambda_positivity,
+        )
         updates, opt_state = optimizer_provider.update(grads, opt_state, params)
         params = optax.apply_updates(params, updates)
         return params, opt_state, loss_value
@@ -175,11 +213,38 @@ def monte_carlo_fit(
         for _ in range(num_batches):
             batch = next(batches)
 
-            parameters, opt_state, loss_value = step(parameters, opt_state, batch)
+            parameters, opt_state, loss_value = step(
+                parameters,
+                opt_state,
+                batch,
+                fast_kernel_arrays,
+                positivity_fast_kernel_arrays,
+                alpha,
+                lambda_positivity,
+            )
 
-            epoch_loss += loss_training(parameters, batch) / batch_size
+            epoch_loss += (
+                loss_training(
+                    parameters,
+                    batch,
+                    fast_kernel_arrays,
+                    positivity_fast_kernel_arrays,
+                    alpha,
+                    lambda_positivity,
+                )
+                / batch_size
+            )
 
-        epoch_val_loss += loss_validation(parameters) / len_val_idx
+        epoch_val_loss += (
+            loss_validation(
+                parameters,
+                fast_kernel_arrays,
+                positivity_fast_kernel_arrays,
+                alpha,
+                lambda_positivity,
+            )
+            / len_val_idx
+        )
         epoch_loss /= num_batches
 
         early_stopper = early_stopper.update(epoch_val_loss)
@@ -200,7 +265,40 @@ def monte_carlo_fit(
 
     log.info("MONTE CARLO RUNNING TIME: %f" % (t1 - t0))
 
-    df = pd.DataFrame(parameters, index=pdf_model.param_names).T
+    return MonteCarloFit(
+        monte_carlo_specs={
+            "max_epochs": max_epochs,
+            "batch_size": batch_size,
+            "batch_seed": batch_seed,
+            "alpha": alpha,
+            "lambda_positivity": lambda_positivity,
+        },
+        training_loss=jnp.array(loss),
+        validation_loss=jnp.array(val_loss),
+        optimized_parameters=parameters,
+    )
+
+
+def run_monte_carlo_fit(monte_carlo_fit, pdf_model, output_path, replica_index):
+    """
+    Runs the Monte Carlo fit and writes the output to the output directory.
+
+    Parameters
+    ----------
+    monte_carlo_fit: MonteCarloFit
+        The results of the Monte Carlo fit.
+
+    pdf_model: pdf_model.PDFModel
+        The PDF model used in the fit.
+
+    output_path: pathlib.PosixPath
+        Path to the output folder.
+
+    replica_index: int
+    """
+    mc_fit = monte_carlo_fit
+
+    df = pd.DataFrame(mc_fit.optimized_parameters, index=pdf_model.param_names).T
 
     # In a Monte Carlo fit, replicas are written to the fit_replicas
     # directory, and mc_postfit must then be applied to select valid ones
@@ -229,25 +327,13 @@ def monte_carlo_fit(
     # Save the training and validation loss
     df = pd.DataFrame(
         {
-            "epochs": range(len(loss)),
-            "training_loss": loss,
-            "validation_loss": val_loss,
+            "epochs": range(len(mc_fit.training_loss)),
+            "training_loss": mc_fit.training_loss,
+            "validation_loss": mc_fit.validation_loss,
         }
     )
     df.to_csv(
         str(output_path) + f"/fit_replicas/replica_{replica_index}" + "/mc_loss.csv",
         index=False,
         float_format="%.5e",
-    )
-
-    return MonteCarloFit(
-        monte_carlo_specs={
-            "max_epochs": max_epochs,
-            "batch_size": batch_size,
-            "batch_seed": batch_seed,
-            "alpha": alpha,
-            "lambda_positivity": lambda_positivity,
-        },
-        training_loss=jnp.array(loss),
-        validation_loss=jnp.array(val_loss),
     )
