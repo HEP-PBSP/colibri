ce49fc8a67a85928776fe3dfdfc738914918645b
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-10-22 14:11:43 +0100

    added comment on atol

--- colibri/theory_predictions.py
+++ colibri/theory_predictions.py
@@ -100,6 +100,7 @@ def make_dis_prediction(fktable, FIT_XGRID, flavour_indices=None):
 
     # Extract xgrid of the FK table and find the indices
     fk_xgrid = fktable.xgrid
+    # atol is chosen to be 1e-7 as this is the order of magnitude of the difference between the smallest entries of the XGRID
     fk_xgrid_indices = closest_indices(FIT_XGRID, fk_xgrid, atol=1e-7)
 
     def dis_prediction(pdf, fk_arr):
@@ -160,6 +161,7 @@ def make_had_prediction(fktable, FIT_XGRID, flavour_indices=None):
 
     # Extract xgrid of the FK table and find the indices
     fk_xgrid = fktable.xgrid
+    # atol is chosen to be 1e-7 as this is the order of magnitude of the difference between the smallest entries of the XGRID
     fk_xgrid_indices = closest_indices(FIT_XGRID, fk_xgrid, atol=1e-7)
 
     def had_prediction(pdf, fk_arr):
