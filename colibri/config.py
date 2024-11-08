e7757adbe76c8580798fcccb1ec5e1f61cccc8d2
Mark Nestor Costantini <mnc33@cam.ac.uk>
2024-11-08 12:25:06 +0000

    ns settings as production rule form colibri_spec

--- colibri/config.py
+++ colibri/config.py
@@ -21,6 +21,7 @@ from colibri.core import (
     ColibriPriorSpecs,
     ColibriSpecs,
 )
+from colibri.config_utils import ns_settings_parser
 
 from mpi4py import MPI
 from reportengine.configparser import ConfigError, explicit_node
@@ -158,7 +159,7 @@ class colibriConfig(Config):
         )
         return xgrid
 
-    def parse_colibri_specs(self, settings):
+    def parse_colibri_specs(self, settings, output_path):
         """
         This parse rule is used to parse the colibri specs that are common to all fits
         and always needed.
@@ -190,10 +191,16 @@ class colibriConfig(Config):
         prior_specs_settings = settings.get("prior_settings", {})
         prior_specs = ColibriPriorSpecs(prior_settings=prior_specs_settings)
 
+        # Nested Sampling settings
+        ns_settings_settings = settings.get("ns_settings", {})
+        ns_settings = ns_settings_parser(ns_settings_settings, output_path)
+
+
         # create a colibri_specs instance
         col_spec = ColibriSpecs(
             loss_function_specs=loss_function_specs,
             prior_settings=prior_specs,
+            ns_settings=ns_settings,
         )
 
         return col_spec
@@ -216,100 +223,14 @@ class colibriConfig(Config):
         """
         return colibri_specs.loss_function_specs.use_fit_t0
 
-    def parse_ns_settings(
+    def produce_ns_settings(
         self,
-        settings,
-        output_path,
+        colibri_specs,
     ):
-        """For a Nested Sampling fit, parses the ns_settings namespace from the runcard,
-        and ensures the choice of settings is valid.
         """
-
-        # Begin by checking that the user-supplied keys are known; warn the user otherwise.
-        known_keys = {
-            "n_posterior_samples",
-            "posterior_resampling_seed",
-            "ReactiveNS_settings",
-            "Run_settings",
-            "SliceSampler_settings",
-            "ultranest_seed",
-            "sampler_plot",
-            "popstepsampler",
-        }
-
-        kdiff = settings.keys() - known_keys
-        for k in kdiff:
-            log.warning(
-                ConfigError(f"Key '{k}' in ns_settings not known.", k, known_keys)
-            )
-
-        # Now construct the ns_settings dictionary, checking the parameter combinations are
-        # valid
-        ns_settings = {}
-
-        # Set the ultranest seed
-        ns_settings["ultranest_seed"] = settings.get("ultranest_seed", 123456)
-
-        # Set the posterior resampling parameters
-        ns_settings["n_posterior_samples"] = settings.get("n_posterior_samples", 1000)
-        ns_settings["posterior_resampling_seed"] = settings.get(
-            "posterior_resampling_seed", 123456
-        )
-
-        # Parse internal settings, if they are not mentioned, set to empty dict
-        ns_settings["ReactiveNS_settings"] = settings.get("ReactiveNS_settings", {})
-        ns_settings["Run_settings"] = settings.get("Run_settings", {})
-        ns_settings["SliceSampler_settings"] = settings.get("SliceSampler_settings", {})
-
-        # set sampler plot to True by default
-        ns_settings["sampler_plot"] = settings.get("sampler_plot", True)
-
-        # set popstepsampler to False by default
-        ns_settings["popstepsampler"] = settings.get("popstepsampler", False)
-
-        # Check that the ReactiveNS_settings key was provided, if not set to default
-        if ns_settings["ReactiveNS_settings"]:
-            # Set the directory where the ultranest logs will be stored; by default
-            # they are stored in output_path/ultranest_logs
-            ns_settings["ReactiveNS_settings"]["log_dir"] = settings[
-                "ReactiveNS_settings"
-            ].get("log_dir", str(output_path / "ultranest_logs"))
-
-            ns_settings["ReactiveNS_settings"]["resume"] = settings[
-                "ReactiveNS_settings"
-            ].get("resume", False)
-
-            ns_settings["ReactiveNS_settings"]["vectorized"] = settings[
-                "ReactiveNS_settings"
-            ].get("vectorized", False)
-        else:
-            ns_settings["ReactiveNS_settings"]["log_dir"] = str(
-                output_path / "ultranest_logs"
-            )
-            ns_settings["ReactiveNS_settings"]["resume"] = False
-            ns_settings["ReactiveNS_settings"]["vectorized"] = False
-
-        # In the case that the fit is resuming from a previous ultranest fit, the logs
-        # directory must exist
-        if ns_settings["ReactiveNS_settings"]["resume"]:
-            if not os.path.exists(ns_settings["ReactiveNS_settings"]["log_dir"]):
-                raise FileNotFoundError(
-                    "Could not find previous ultranest fit at "
-                    + str(ns_settings["ReactiveNS_settings"]["log_dir"])
-                    + "."
-                )
-
-            log.info(
-                "Resuming ultranest fit from "
-                + str(ns_settings["ReactiveNS_settings"]["log_dir"])
-                + "."
-            )
-
-        # If the resume option is false, ultranest expects "overwrite" instead
-        if not ns_settings["ReactiveNS_settings"]["resume"]:
-            ns_settings["ReactiveNS_settings"]["resume"] = "overwrite"
-
-        return ns_settings
+        Given the parsed colibri_specs, returns the ns_settings.
+        """
+        return colibri_specs.ns_settings.ns_settings
 
     def parse_positivity_penalty_settings(self, settings):
         """
