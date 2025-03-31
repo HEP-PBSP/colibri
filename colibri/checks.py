72024adb569ff73d11dd026ef3714ca8f1fababa
Luca Mantani <luca.mantani@gmail.com>
2025-03-31 15:44:27 +0200

    removed t0pdfset from check

--- colibri/checks.py
+++ colibri/checks.py
@@ -13,7 +13,7 @@ from colibri.utils import get_fit_path, get_pdf_model, pdf_models_equal
 
 
 @make_argcheck
-def check_pdf_models_equal(prior_settings, pdf_model, theoryid, t0pdfset):
+def check_pdf_models_equal(prior_settings, pdf_model, theoryid):
     """
     Decorator that can be added to functions to check that the
     PDF model used as prior (eg when using prior_settings["type"] == "prior_from_gauss_posterior")
@@ -38,12 +38,6 @@ def check_pdf_models_equal(prior_settings, pdf_model, theoryid, t0pdfset):
         if prior_filter["theoryid"] != theoryid.id:
             raise ValueError(
                 f"Theory id {theoryid} does not match theory id of prior {prior_filter['theoryid']}"
-            )
-
-        # check that t0pdfset used in prior fit is the same as the one used in the current fit
-        if prior_filter["t0pdfset"] != t0pdfset.name:
-            raise ValueError(
-                f"t0pdfset {theoryid.t0pdfset} does not match t0pdfset of prior {prior_filter['t0pdfset']}"
             )
 
 
