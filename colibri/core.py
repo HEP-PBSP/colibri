65a527dc2b929f09b9b080042d85cf107d9e84af
Mark Nestor Costantini <mnc33@cam.ac.uk>
2025-03-31 14:21:40 +0100

    fixed docs of core.py

--- colibri/core.py
+++ colibri/core.py
@@ -45,8 +45,9 @@ class IntegrabilitySettings:
     -------
     integrability_settings:
         integrability: True
-        evolution_flavours: [V, V3, V8, T3, T8]
-        lambda_integrability: 1000
+        integrability_specs:
+            evolution_flavours: [V, V3, V8, T3, T8]
+            lambda_integrability: 1000
     """
 
     integrability: bool
