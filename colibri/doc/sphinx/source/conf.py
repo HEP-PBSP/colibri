f62620fac6b3cca2d89e165f68bdffab649cb289
vschutze-alt <valentinaschutze@hotmail.com>
2025-04-03 16:34:07 +0100

    Added theory on the Les Houches Parametrisation

--- colibri/doc/sphinx/source/conf.py
+++ colibri/doc/sphinx/source/conf.py
@@ -93,7 +93,7 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"logo_only": True, "display_version": False}
+html_theme_options = {"logo_only": True}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -214,3 +214,5 @@ intersphinx_mapping = {"python": ("https://docs.python.org/", None)}
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+
