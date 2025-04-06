fd01dfdf684211afe1e82bb61c1ce465073c0a92
Mark Nestor Costantini <mnc33@cam.ac.uk>
2025-04-06 16:20:59 +0100

    added symlinking

--- colibri/scripts/evolve_fit.py
+++ colibri/scripts/evolve_fit.py
@@ -3,18 +3,35 @@ A wrapper around n3fit/scripts/evolven3fit.py.
 
 """
 import os
+import sys
+
 from n3fit.scripts.evolven3fit import main as evolven3fit_main
+
 
 def main():
     """
     Before running `evolven3fit` from n3fit/scripts/evolven3fit.py,
     creates a symlink called `nnfit` to replicas folder.
     """
-    # Create a symlink to the replicas folder
-    os.symlink('replicas', 'nnfit')
+    if len(sys.argv) != 3:
+        print("Usage: evolve_fit <command> name_fit")
+        sys.exit(1)
     
-    # Import and run the main function from evolven3fit
-    
-    evolven3fit_main()
-    # Remove the symlink after running
-    os.remove('nnfit')
+    fit_dir = sys.argv[2]
+    replicas_path = os.path.join(fit_dir, "replicas")
+    symlink_path = os.path.join(fit_dir, "nnfit")
+
+    if not os.path.exists(replicas_path):
+        print(f"Error: replicas folder not found at {replicas_path}")
+        sys.exit(1)
+
+    try:
+        os.symlink("replicas", symlink_path)
+    except FileExistsError:
+        print(f"Warning: symlink {symlink_path} already exists")
+
+    try:
+        evolven3fit_main()
+    finally:
+        if os.path.islink(symlink_path):
+            os.remove(symlink_path)
