391356683133b8210bf4a0d8b5a9fb88697b88fd
Mark Nestor Costantini <mnc33@cam.ac.uk>
2025-05-09 16:42:05 +0100

    use MOCK_PDF_MODEL and TEST_XGRID in test_closure_test

--- colibri/tests/test_closure_test.py
+++ colibri/tests/test_closure_test.py
@@ -1,4 +1,6 @@
 """
+colibri.tests.test_closure_test
+
 Module for testing the closure test functions in the colibri package.
 """
 
@@ -15,11 +17,13 @@ from colibri.closure_test import (
     closure_test_colibri_model_pdf,
     closure_test_pdf_grid,
 )
-
-
-@pytest.fixture
-def sample_xgrid():
-    return jnp.array([0.1, 0.2, 0.3, 0.4, 0.5])
+from colibri.tests.conftest import (
+    CLOSURE_TEST_PDFSET,
+    MOCK_PDF_MODEL,
+    TEST_N_FL,
+    TEST_N_XGRID,
+    TEST_XGRID,
+)
 
 
 @pytest.fixture
@@ -38,18 +42,13 @@ def test_closure_test_pdf_grid():
     - Output type is a jnp.array.
     - The output shape is (N_rep, N_fl, N_x)
     """
-    # mock a valid PDF set
-    inp = {"closure_test_pdf": "NNPDF40_nlo_as_01180"}
-    cltest_pdf_set = cAPI.closure_test_pdf(**inp)
+    cltest_pdf_set = cAPI.closure_test_pdf(**CLOSURE_TEST_PDFSET)
 
     # Check 1: closure_test_pdf is an instance of validphys.core.PDF
     assert isinstance(cltest_pdf_set, validphys.core.PDF)
 
-    # define a test array
-    FIT_XGRID = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-
     # call the function
-    grid = closure_test_pdf_grid(cltest_pdf_set, FIT_XGRID, Q0=1.65)
+    grid = closure_test_pdf_grid(cltest_pdf_set, TEST_XGRID, Q0=1.65)
 
     # Check 2: type of the output is a jnp.array
     assert isinstance(grid, jnp.ndarray)
@@ -58,43 +57,25 @@ def test_closure_test_pdf_grid():
     N_rep = cltest_pdf_set.get_members()  #   number of replicas
     N_fl = len(convolution.FK_FLAVOURS)  # number of flavours
 
-    print(f"FK_FLAVOURS: {convolution.FK_FLAVOURS}")
-
-    assert grid.shape == (N_rep, N_fl, len(FIT_XGRID))
-
-
-@pytest.fixture
-def mock_colibri_model():
-    model = MagicMock()
-    model.grid_values_func = MagicMock(
-        return_value=lambda params: jnp.array(
-            [[p * x for x in range(1, 6)] for p in params]
-        )
-    )
-    return model
+    assert grid.shape == (N_rep, N_fl, TEST_N_XGRID)
 
 
 @patch("colibri.closure_test.pdf_model_from_colibri_model")
-def test_closure_test_pdf_grid_with_colibri_model(
-    mock_pdf_model_from_colibri_model, sample_xgrid, mock_colibri_model
-):
+def test_closure_test_pdf_grid_with_colibri_model(mock_pdf_model_from_colibri_model):
     # Mock the pdf model
-    mock_pdf_model_from_colibri_model.return_value = mock_colibri_model
+    mock_pdf_model_from_colibri_model.return_value = MOCK_PDF_MODEL
 
     settings = {"parameters": [1, 2, 3], "model": "test_model"}
     grid = closure_test_pdf_grid(
-        "colibri_model", sample_xgrid, closure_test_model_settings=settings
+        "colibri_model", TEST_XGRID, closure_test_model_settings=settings
     )
-    assert grid.shape == (1, 3, 5)
-    assert jnp.array_equal(grid[0][0], jnp.array([1, 2, 3, 4, 5]))
+    assert grid.shape == (1, TEST_N_FL, TEST_N_XGRID)
 
 
 @patch("validphys.convolution.evolution.grid_values")
-def test_closure_test_pdf_grid_with_pdf_object(
-    mock_grid_values, sample_xgrid, mock_pdf
-):
+def test_closure_test_pdf_grid_with_pdf_object(mock_grid_values, mock_pdf):
     mock_grid_values.return_value = mock_pdf.grid_values
-    grid = closure_test_pdf_grid(mock_pdf, sample_xgrid)
+    grid = closure_test_pdf_grid(mock_pdf, TEST_XGRID)
     assert grid.shape == (1, 13, 5)
     assert jnp.all(grid == 1)
 
@@ -108,11 +89,10 @@ def test_closure_test_central_pdf_grid():
 
 @patch("colibri.closure_test.pdf_model_from_colibri_model")
 def test_closure_test_colibri_model_pdf(
-    mock_pdf_model_from_colibri_model, sample_xgrid, mock_colibri_model
+    mock_pdf_model_from_colibri_model,
 ):
     # Mock the pdf model
-    mock_pdf_model_from_colibri_model.return_value = mock_colibri_model
+    mock_pdf_model_from_colibri_model.return_value = MOCK_PDF_MODEL
     settings = {"parameters": [1, 2, 3], "model": "test_model"}
-    pdf_grid = closure_test_colibri_model_pdf(settings, sample_xgrid)
-    assert pdf_grid.shape == (3, 5)
-    assert jnp.array_equal(pdf_grid[0], jnp.array([1, 2, 3, 4, 5]))
+    pdf_grid = closure_test_colibri_model_pdf(settings, TEST_XGRID)
+    assert pdf_grid.shape == (TEST_N_FL, TEST_N_XGRID)
