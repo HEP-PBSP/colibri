bb4db5b4bac1ed20a7038558112371319323a7ed
Mark Nestor Costantini <mnc33@cam.ac.uk>
2025-05-21 16:49:18 +0100

    added tests for FLAVOUR TO ID and ID to FLAVOUR

--- colibri/tests/test_constants.py
+++ colibri/tests/test_constants.py
@@ -1,6 +1,51 @@
 """
 colibri.tests.test_constants.py
 
-Module for testing that the constants in the colibri module are 
+Module for testing that the constants in the colibri module are
 working as expected.
 """
+
+from colibri.constants import FLAVOURS_ID_MAPPINGS, FLAVOUR_TO_ID_MAPPING
+
+
+def test_id_to_flavour_mappings():
+    """
+    Test that the id to flavour mapping is correct.
+    """
+
+    assert FLAVOURS_ID_MAPPINGS[0] == "photon"
+    assert FLAVOURS_ID_MAPPINGS[1] == "\Sigma"
+    assert FLAVOURS_ID_MAPPINGS[2] == "g"
+    assert FLAVOURS_ID_MAPPINGS[3] == "V"
+    assert FLAVOURS_ID_MAPPINGS[4] == "V3"
+    assert FLAVOURS_ID_MAPPINGS[5] == "V8"
+    assert FLAVOURS_ID_MAPPINGS[6] == "V15"
+    assert FLAVOURS_ID_MAPPINGS[7] == "V24"
+    assert FLAVOURS_ID_MAPPINGS[8] == "V35"
+    assert FLAVOURS_ID_MAPPINGS[9] == "T3"
+    assert FLAVOURS_ID_MAPPINGS[10] == "T8"
+    assert FLAVOURS_ID_MAPPINGS[11] == "T15"
+    assert FLAVOURS_ID_MAPPINGS[12] == "T24"
+    assert FLAVOURS_ID_MAPPINGS[13] == "T35"
+
+
+def test_flavour_to_id_mapping():
+    """
+    Test that the flavour to ID mapping is correct.
+    """
+
+    # Check that the mapping is correct
+    assert FLAVOUR_TO_ID_MAPPING["photon"] == 0
+    assert FLAVOUR_TO_ID_MAPPING["\Sigma"] == 1
+    assert FLAVOUR_TO_ID_MAPPING["g"] == 2
+    assert FLAVOUR_TO_ID_MAPPING["V"] == 3
+    assert FLAVOUR_TO_ID_MAPPING["V3"] == 4
+    assert FLAVOUR_TO_ID_MAPPING["V8"] == 5
+    assert FLAVOUR_TO_ID_MAPPING["V15"] == 6
+    assert FLAVOUR_TO_ID_MAPPING["V24"] == 7
+    assert FLAVOUR_TO_ID_MAPPING["V35"] == 8
+    assert FLAVOUR_TO_ID_MAPPING["T3"] == 9
+    assert FLAVOUR_TO_ID_MAPPING["T8"] == 10
+    assert FLAVOUR_TO_ID_MAPPING["T15"] == 11
+    assert FLAVOUR_TO_ID_MAPPING["T24"] == 12
+    assert FLAVOUR_TO_ID_MAPPING["T35"] == 13
