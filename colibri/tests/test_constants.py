e45c1ca6ea4f52a079c4ecaf5cf2072ad36afc48
Mark Nestor Costantini <mnc33@cam.ac.uk>
2025-05-21 18:11:40 +0100

    added test for some valence flavours

--- colibri/tests/test_constants.py
+++ colibri/tests/test_constants.py
@@ -88,6 +88,15 @@ def test_LHAPDF_XGRID():
 def test_evolution_to_flavour_matrix():
     """
     Tests that the evolution to flavour matrix rotation is correct.
+
+    NOTE: testing assumes the following ordering of the flavours:
+
+    Evolution basis:
+    [photon, sigma, gluon, V, V3, V8, V15, V24, V35, T3, T8, T15, T24, T35]
+
+    Flavour basis:
+    [TBAR, BBAR, CBAR, SBAR, UBAR, DBAR, gluon, D, U, S, C, B, T, photon]
+
     """
 
     # photon basis vector in the evolution basis
@@ -119,3 +128,74 @@ def test_evolution_to_flavour_matrix():
         ]
     )
     assert np.allclose(evolution_to_flavour_matrix @ sigma_ev, sigma_fl)
+
+    # gluon basis vector in the evolution basis
+    gluon_ev = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    # gluon basis vector in the flavour basis
+    gluon_fl = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+
+    assert np.allclose(evolution_to_flavour_matrix @ gluon_ev, gluon_fl)
+
+    # V basis vector in the evolution basis
+    V_ev = np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    # V basis vector in the flavour basis
+    V_fl = np.array(
+        [
+            -1 / 12,
+            -1 / 12,
+            -1 / 12,
+            -1 / 12,
+            -1 / 12,
+            -1 / 12,
+            0,
+            1 / 12,
+            1 / 12,
+            1 / 12,
+            1 / 12,
+            1 / 12,
+            1 / 12,
+            0,
+        ]
+    )
+
+    assert np.allclose(evolution_to_flavour_matrix @ V_ev, V_fl)
+
+    # V3 basis vector in the evolution basis
+    V3_ev = np.array([0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    # V3 basis vector in the flavour basis
+    V3_fl = np.array([0, 0, 0, 0, -0.25, 0.25, 0, -0.25, 0.25, 0, 0, 0, 0, 0])
+
+    assert np.allclose(evolution_to_flavour_matrix @ V3_ev, V3_fl)
+
+    # V8 basis vector in the evolution basis
+    V8_ev = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+    # V8 basis vector in the flavour basis
+    V8_fl = np.array(
+        [0, 0, 0, 1 / 6, -1 / 12, -1 / 12, 0, 1 / 12, 1 / 12, -1 / 6, 0, 0, 0, 0]
+    )
+
+    assert np.allclose(evolution_to_flavour_matrix @ V8_ev, V8_fl)
+
+    # V15 basis vector in the evolution basis
+    V15_ev = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+
+    # V15 basis vector in the flavour basis
+    V15_fl = np.array(
+        [
+            0,
+            0,
+            0.125,
+            -1 / 24,
+            -1 / 24,
+            -1 / 24,
+            0,
+            1 / 24,
+            1 / 24,
+            1 / 24,
+            -0.125,
+            0,
+            0,
+            0,
+        ]
+    )
+    assert np.allclose(evolution_to_flavour_matrix @ V15_ev, V15_fl)
