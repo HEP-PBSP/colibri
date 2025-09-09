d092ca42569ff7d45ec86fe0c92dca939607993c
Luca Mantani <luca.mantani@gmail.com>
2025-09-09 09:52:28 +0200

    Moved vectorisation to ultranest_fit

--- colibri/ultranest_fit.py
+++ colibri/ultranest_fit.py
@@ -92,6 +92,9 @@ def ultranest_fit(
 
     parameters = pdf_model.param_names
 
+    if ns_settings["ReactiveNS_settings"]["vectorized"]:
+        log_likelihood = jax.vmap(log_likelihood, in_axes=(0,), out_axes=0)
+
     sampler = ultranest.ReactiveNestedSampler(
         parameters,
         log_likelihood,
