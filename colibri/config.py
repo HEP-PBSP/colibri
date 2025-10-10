a706d5d0eac467bb608d6b7c41cba053a1b28ea5
vschutze-alt <valentinaschutze@hotmail.com>
2025-10-10 16:19:36 +0100

    fix typo in config.py

--- colibri/config.py
+++ colibri/config.py
@@ -268,7 +268,8 @@ class colibriConfig(Config):
             "repeats",
             "delete_fraction",
             "log_precision",
-            "posterior_resampling_seed" "seed",
+            "posterior_resampling_seed",
+            "seed",
         }
 
         kdiff = settings.keys() - known_keys
