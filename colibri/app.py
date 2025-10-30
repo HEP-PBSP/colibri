4a71bd62214d7080bbf4eed99596f7192ec56334
vschutze-alt <valentinaschutze@hotmail.com>
2025-10-30 11:28:18 +0000

    add module to time the log likelihood for different sample sizes

--- colibri/app.py
+++ colibri/app.py
@@ -12,6 +12,7 @@ import pathlib
 colibri_providers = [
     "colibri.theory_predictions",
     "colibri.theory_penalties",
+    "colibri.time_likelihood",
     "colibri.loss_functions",
     "colibri.mc_loss_functions",
     "colibri.optax_optimizer",
