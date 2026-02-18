d8cbb4859708b127d21d6db03ffa51f1185c32a1
Ella Cole <ellagcole02@gmail.com>
2026-02-18 15:10:20 +0000

    typo

--- colibri/mc_utils.py
+++ colibri/mc_utils.py
@@ -28,7 +28,7 @@ def mc_pseudodata(
     pseudodata_central_covmat_index,
     replica_index,
     trval_seed,
-    mc_seed,
+    mcseed,
     shuffle_indices=True,
     mc_validation_fraction=0.2,
 ):
