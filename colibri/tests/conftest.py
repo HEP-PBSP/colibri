0803f6abefb120c049f81dcf887de5838f640998
Mark Nestor Costantini <mnc33@cam.ac.uk>
2026-03-20 10:19:14 +0100

    remove MOCK of central inv covmat from conftest

--- colibri/tests/conftest.py
+++ colibri/tests/conftest.py
@@ -340,17 +340,12 @@ Function expects a tuple of DIS-like fast kernel array of shape (N_data, TEST_N_
 """
 
 
-MOCK_CENTRAL_INV_COVMAT_INDEX = Mock()
-MOCK_CENTRAL_INV_COVMAT_INDEX.central_values = jnp.ones(TEST_N_DATA)
-MOCK_CENTRAL_INV_COVMAT_INDEX.inv_covmat = jnp.eye(TEST_N_DATA)
-MOCK_CENTRAL_INV_COVMAT_INDEX.central_values_idx = jnp.arange(TEST_N_DATA)
-
 MOCK_CENTRAL_COVMAT_INDEX = Mock()
 MOCK_CENTRAL_COVMAT_INDEX.central_values = jnp.ones(TEST_N_DATA)
 MOCK_CENTRAL_COVMAT_INDEX.covmat = jnp.eye(TEST_N_DATA)
 MOCK_CENTRAL_COVMAT_INDEX.central_values_idx = jnp.arange(TEST_N_DATA)
 """
-Mock instance of Central Inverse covmat index object.
+Mock instance of Central covmat index object.
 """
 
 
