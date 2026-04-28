28daeabc225bfeae838fbfe9331a4420c63d400d
Luca Mantani <luca.mantani@gmail.com>
2026-04-28 15:02:35 +0200

    Adapted after merging main

--- colibri/bayes_prior.py
+++ colibri/bayes_prior.py
@@ -43,8 +43,9 @@ def bayesian_prior(prior_settings, forward_map):
 
         elif "min_val" in prior_specs and "max_val" in prior_specs:
             # Global bounds for all parameters
-            mins = jnp.array([float(prior_specs["min_val"])] * pdf_model.n_parameters)
-            maxs = jnp.array([float(prior_specs["max_val"])] * pdf_model.n_parameters)
+            n_params = len(forward_map.param_names)
+            mins = jnp.array([float(prior_specs["min_val"])] * n_params)
+            maxs = jnp.array([float(prior_specs["max_val"])] * n_params)
 
         else:
             raise ValueError(
